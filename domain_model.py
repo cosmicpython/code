44f51636afec605a25aab3be7d4c141672bb16d7
Harry <hjwp2@cantab.net>
2019-01-26 00:36:53 +0000

    try to factor out common allocate helper fns

--- domain_model.py
+++ domain_model.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from datetime import date
 
 @dataclass
 class Line:
@@ -13,55 +12,57 @@ class OrderLine(Line):
 
 
 @dataclass
-class Shipment:
-    id: str
-    eta: date
-    lines: list
+class Shipment(list):
+    def __init__(self, id, eta, lines):
+        self.id = id
+        self.eta = eta
+        super().__init__(lines)
 
 
 def skus(thing):
-    try:
-        return {line.sku for line in thing}
-    except TypeError:
-        return {line.sku for line in thing.lines}
+    return {line.sku for line in thing}
 
 
-def allocate_to(line, allocation, quantities):
-    for quantity in quantities:
-        if quantity.sku == line.sku and quantity.quantity > line.quantity:
+def allocate_to(line, allocation, source):
+    for source_line in source:
+        if source_line.sku == line.sku and source_line.quantity > line.quantity:
             line.allocation = allocation
-            quantity.quantity -= line.quantity
+            source_line.quantity -= line.quantity
             return
 
-def allocate_to_stock(line, stock):
-    allocate_to(line, 'STOCK', stock)
-
-def allocate_to_shipment(line, shipment):
-    allocate_to(line, shipment.id, shipment.lines)
+def allocate_to_stock(order, stock):
+    for line in order:
+        allocate_to(line, 'STOCK', stock)
 
 
-def allocate_to_shipments(line, shipments):
+def allocate_to_shipment(order, shipment):
+    for line in order:
+        allocate_to(line, shipment.id, shipment)
+
+
+def allocate_to_shipments(order, shipments):
+    for line in order:
+        _allocate_line_to_shipments(line, shipments)
+
+
+def _allocate_line_to_shipments(line, shipments):
     for shipment in shipments:
-        allocate_to_shipment(line, shipment)
-        if line.allocation is not None:
+        allocate_to(line, shipment.id, shipment)
+        if line.allocation:
             return
 
 
 def allocate(order, stock, shipments):
     if skus(order) <= skus(stock):
-        for line in order:
-            allocate_to_stock(line, stock)
+        allocate_to_stock(order, stock)
         return
 
     shipments.sort(key=lambda s: s.eta)
 
     for shipment in shipments:
         if skus(order) <= skus(shipment):
-            for line in order:
-                allocate_to(line, shipment.id, shipment.lines)
+            allocate_to_shipment(order, shipment)
             return
 
-    for line in order:
-        allocate_to_stock(line, stock)
-        if line.allocation is None:
-            allocate_to_shipments(line, shipments)
+    allocate_to_shipments(order, shipments)
+    allocate_to_stock(order, stock)

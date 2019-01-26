27b3362efd5ab62e20249277a7a954e02bfc7c45
Harry <hjwp2@cantab.net>
2019-01-26 03:49:09 +0000

    this is getting silly now. where's all my code gone?

--- domain_model.py
+++ domain_model.py
@@ -14,20 +14,22 @@ def allocate_to(order, source):
     }
 
 
+def merge(allocations):
+    return {
+        k: v
+        for d in allocations
+        for k, v in d.items()
+    }
+
+
+
 def allocate(order, stock, shipments):
-    stock_allocation = allocate_to(order, stock)
-    if allocated_completely(order, stock_allocation):
-        return stock_allocation
+    allocations = []
+    for source in [stock] + shipments:
+        allocation = allocate_to(order, source)
+        if allocated_completely(order, allocation):
+            return allocation
+        allocations.append(allocation)
 
-    shipment_allocations = []
-    for shipment in shipments:
-        shipment_allocation = allocate_to(order, shipment)
-        if allocated_completely(order, shipment_allocation):
-            return shipment_allocation
-        shipment_allocations.append(shipment_allocation)
-
-    mixed_allocation = {}
-    for allocation in shipment_allocations + [stock_allocation]:
-        mixed_allocation.update(allocation)
-    return mixed_allocation
+    return merge(reversed(allocations))
 

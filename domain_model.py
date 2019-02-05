d1af5d106ff2479d92f98a1cd5304ff943cca7e9
Harry <hjwp2@cantab.net>
2019-02-05 09:58:17 +0000

    add a skus property

--- domain_model.py
+++ domain_model.py
@@ -1,25 +1,36 @@
 from dataclasses import dataclass
 from datetime import date
 
+class Allocation(dict):
+
+    @property
+    def skus(self):
+        return self.keys()
+
+
 @dataclass
 class Order:
     lines: dict
     allocations: dict = None
 
     @property
+    def skus(self):
+        return self.lines.keys()
+
+    @property
     def fully_allocated(self):
-        return set(self.allocations.keys()) == set(self.lines.keys())
+        return self.allocations.skus == self.skus
 
 
     def allocate(self, stock, shipments):
         self.allocations = {}
         for source in [stock] + shipments:
-            allocation = {
+            allocation = Allocation({
                 sku: source
                 for sku, quantity in self.lines.items()
                 if source.can_allocate(sku, quantity)
-            }
-            if set(allocation.keys()) == set(self.lines):
+            })
+            if allocation.skus == self.skus:
                 self.allocations = allocation
                 return
             allocation.update(self.allocations)
@@ -33,5 +44,4 @@ class Stock:
 
     def can_allocate(self, sku, quantity):
         return sku in self.lines and self.lines[sku] > quantity
-
 

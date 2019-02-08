4219cddd60c0b6cffb1744bb53473d9d98b74fab
Harry <hjwp2@cantab.net>
2019-02-08 15:52:26 +0000

    strip down solution

--- domain_model.py
+++ domain_model.py
@@ -1,26 +1,13 @@
 def allocate(order, stock, shipments):
-    allocations = []
-    for source in [stock] + shipments:
-        allocation = allocate_to(order, source)
-        if allocated_completely(order, allocation):
-            return allocation
-        allocations.append(allocation)
-    return combine_preferring_first(allocations)
+    allocation = {}
+    for source in shipments + [stock]:
+        allocation.update(allocation_from(order, source))
+    return allocation
 
-def allocate_to(order, source):
+def allocation_from(order, source):
     return {
         sku: source
         for sku, quantity in order.items()
         if sku in source
         and source[sku] > quantity
-    }
-
-def allocated_completely(order, allocation):
-    return order.keys() == allocation.keys()
-
-def combine_preferring_first(allocations):
-    return {
-        k: v
-        for d in reversed(allocations)
-        for k, v in d.items()
     }

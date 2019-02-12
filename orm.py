b268531431b87db9030bbd60ed257031b4d990d0
Harry <hjwp2@cantab.net>
2019-02-12 15:42:29 +0000

    some progress on getting the orm to deliver dicts

--- orm.py
+++ orm.py
@@ -1,5 +1,8 @@
 from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import mapper, relationship
+from sqlalchemy.orm import mapper, relationship, column_property
+from sqlalchemy.orm.collections import attribute_mapped_collection, mapped_collection, collection
+
+from domain_model import Order
 
 metadata = MetaData()
 
@@ -7,13 +10,22 @@ metadata = MetaData()
 order = Table(
     'order', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
+    # relationship('order_lines', secondary='order_lines')
 )
 
 order_lines = Table(
     'order_lines', metadata,
     Column('order_id', ForeignKey('order.id'), primary_key=True),
     Column('sku', String(255), primary_key=True),
-    Column('quantity', Integer),
-    # order=relationship(order, back_populates="lines")
+    Column('qty', Integer),
 )
 
+mapper(Order, order)
+
+
+from sqlalchemy import event
+
+@event.listens_for(Order, 'load')
+def custom_load(target, context):
+    raw_lines = context.session.query(order_lines).join(order).filter_by(id=target.id).all()
+    target._lines = {sku: qty for _, sku, qty in raw_lines}
--- test_orm.py
+++ test_orm.py
@@ -3,11 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from orm import metadata
+from domain_model import Order
 
 
 @pytest.fixture
 def db():
-    engine = create_engine('sqlite:///:memory:', echo=True)
+    # engine = create_engine('sqlite:///:memory:', echo=True)
+    engine = create_engine('sqlite:///:memory:')
     metadata.create_all(engine)
     return engine
 
@@ -25,3 +27,17 @@ def test_smoke(session):
         (1, 1, "sku2", 13),
     ]
 
+def test_order_mapper_no_lines(session):
+    order = Order({})
+    session.add(order)
+    assert session.query(Order).first() == order
+
+def test_order_mapper_with_lines(session):
+    session.execute('INSERT INTO "order" VALUES (1)')
+    session.execute('INSERT INTO "order" VALUES (2)')
+    session.execute('INSERT INTO "order_lines" VALUES (1, "sku1", 12)')
+    session.execute('INSERT INTO "order_lines" VALUES (1, "sku2", 13)')
+    session.execute('INSERT INTO "order_lines" VALUES (2, "sku3", 14)')
+    expected_order = Order({'sku1': 12, 'sku2': 13})
+    retrieved_order = session.query(Order).first()
+    assert retrieved_order.lines == expected_order.lines

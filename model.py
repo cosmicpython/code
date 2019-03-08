fc5811e40bae2de7a8f444a6d54f8da72ce4b654
Harry <hjwp2@cantab.net>
2019-03-08 21:47:37 +0000

    first stab at a model [domain_model_1]

---
+++ model.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self.available_quantity = qty
+
+    def allocate(self, line: OrderLine):
+        self.available_quantity -= line.qty

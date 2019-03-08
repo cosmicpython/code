5be7bcfc0763133b384388c160c1ce308eb304ba
Harry <hjwp2@cantab.net>
2019-03-08 21:45:23 +0000

    first test [first_test]

---
+++ test_batches.py
@@ -0,0 +1,11 @@
+from model import Batch, OrderLine
+from datetime import date
+
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
+    line = OrderLine("order-ref", "SMALL-TABLE", 2)
+
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18

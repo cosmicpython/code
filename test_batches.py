f88f278c7f196a0ea23408adb5c0f681d6a3b24f
Harry <hjwp2@cantab.net>
2019-03-08 22:49:55 +0000

    test allocate twice [last_test]

--- test_batches.py
+++ test_batches.py
@@ -34,6 +34,12 @@ def test_cannot_allocate_if_skus_do_not_match():
     line = OrderLine("order-123", 'sku2', 10)
     assert batch.can_allocate(line) is False
 
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("small-table", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
+
 def test_deallocate():
     batch, line = make_batch_and_line("small-table", 20, 2)
     batch.allocate(line)
@@ -44,3 +50,4 @@ def test_can_only_deallocate_allocated_lines():
     batch, line = make_batch_and_line("small-table", 20, 2)
     batch.deallocate(line)
     assert batch.available_quantity == 20
+

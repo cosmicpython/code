92dc6bf46111967c659555c6466418cb7c3b8fed
Harry <hjwp2@cantab.net>
2019-03-08 22:45:38 +0000

    simple deallocate test

--- test_batches.py
+++ test_batches.py
@@ -30,6 +30,12 @@ def test_can_allocate_if_available_equal_to_required():
     assert batch.can_allocate(line)
 
 def test_cannot_allocate_if_skus_do_not_match():
-    batch = Batch("batch-001", 'UNCOMFORTABLE-CHAIR', 100, eta=None)
-    different_sku_line = OrderLine("order-123", 'EXPENSIVE-TOASTER', 10)
+    batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
+    different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sku_line) is False
+
+def test_deallocate():
+    batch, line = make_batch_and_line("EXPENSIVE-FOOTSTOOL", 20, 2)
+    batch.allocate(line)
+    batch.deallocate(line)
+    assert batch.available_quantity == 20

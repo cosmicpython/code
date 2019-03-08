197b5bbdc83eb2d1cc5c430f81ed08cb9b9f2d77
Harry <hjwp2@cantab.net>
2019-03-08 22:22:09 +0000

    more tests for can_allocate [test_can_allocate]

--- test_batches.py
+++ test_batches.py
@@ -1,3 +1,4 @@
+from datetime import date
 from model import Batch, OrderLine
 from datetime import date
 
@@ -8,3 +9,27 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch.allocate(line)
 
     assert batch.available_quantity == 18
+
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty)
+    )
+
+
+def test_can_allocate_if_available_greater_than_required():
+    large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
+    assert large_batch.can_allocate(small_line)
+
+def test_cannot_allocate_if_available_smaller_than_required():
+    small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
+    assert small_batch.can_allocate(large_line) is False
+
+def test_can_allocate_if_available_equal_to_required():
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
+    assert batch.can_allocate(line)
+
+def test_cannot_allocate_if_skus_do_not_match():
+    batch = Batch("batch-001", 'UNCOMFORTABLE-CHAIR', 100, eta=None)
+    different_sku_line = OrderLine("order-123", 'EXPENSIVE-TOASTER', 10)
+    assert batch.can_allocate(different_sku_line) is False

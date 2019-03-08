531d999d825c47c148e07867bea233091c338532
Harry <hjwp2@cantab.net>
2019-03-08 22:49:55 +0000

    test allocate twice [last_test]

--- test_batches.py
+++ test_batches.py
@@ -1,6 +1,6 @@
 from datetime import date
 from model import Batch, OrderLine
-from datetime import date
+
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
@@ -34,6 +34,12 @@ def test_cannot_allocate_if_skus_do_not_match():
     different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sku_line) is False
 
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
+
 def test_deallocate():
     batch, line = make_batch_and_line("EXPENSIVE-FOOTSTOOL", 20, 2)
     batch.allocate(line)
@@ -44,3 +50,4 @@ def test_can_only_deallocate_allocated_lines():
     batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
     batch.deallocate(unallocated_line)
     assert batch.available_quantity == 20
+

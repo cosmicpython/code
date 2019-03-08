1b76c8abed3776f28c0c1fdbc5a18d39e1dd4096
Harry <hjwp2@cantab.net>
2019-03-08 22:48:39 +0000

    model now tracks allocations [domain_model_complete]

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import Optional, Set
 
 
 @dataclass(frozen=True)
@@ -18,13 +18,24 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: Set[OrderLine]
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
 
     def deallocate(self, line: OrderLine):
-        self.available_quantity += line.qty
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty

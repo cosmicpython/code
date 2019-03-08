d817ecd359fd83654a0c594873a6b3a34805e92c
Harry <hjwp2@cantab.net>
2019-03-08 22:25:54 +0000

    can_allocate fn [can_allocate]

--- model.py
+++ model.py
@@ -20,3 +20,6 @@ class Batch:
 
     def allocate(self, line: OrderLine):
         self.available_quantity -= line.qty
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty

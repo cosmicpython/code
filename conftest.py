ef7a621a80b4f956c874ec67bcf10dec6016c7d0
Harry <hjwp2@cantab.net>
2019-03-10 23:51:02 +0000

    first tests of orm [orm_tests]

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+
+from orm import metadata, start_mappers
+
+
+@pytest.fixture
+def in_memory_db():
+    engine = create_engine("sqlite:///:memory:")
+    metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def session(in_memory_db):
+    start_mappers()
+    yield sessionmaker(bind=in_memory_db)()
+    clear_mappers()

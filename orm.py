2a29db0a35ab484c2eda2884416e392cb745648d
Harry <hjwp2@cantab.net>
2019-03-10 23:55:12 +0000

    batches with no allocations

--- orm.py
+++ orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String
+from sqlalchemy import Table, MetaData, Column, Integer, String, Date
 from sqlalchemy.orm import mapper
 
 import model
@@ -13,6 +13,15 @@ order_lines = Table(
     Column('qty', Integer),
 )
 
+batches = Table(
+    'batches', metadata,
+    Column('reference', String(255), primary_key=True),
+    Column('sku', String(255), primary_key=True),
+    Column('_purchased_qty', Integer),
+    Column('eta', Date),
+)
+
 
 def start_mappers():
     mapper(model.OrderLine, order_lines)
+    mapper(model.Batch, batches)

58d05e4c3017cb14ab48d5e4a8fc8809750431c4
Harry <hjwp2@cantab.net>
2019-03-10 23:50:34 +0000

    first cut of orm, orderlines only [sqlalchemy_classical_mapper]

--- test_batches.py
+++ test_batches.py
@@ -17,17 +17,21 @@ def make_batch_and_line(sku, batch_qty, line_qty):
         OrderLine("order-123", sku, line_qty),
     )
 
+
 def test_can_allocate_if_available_greater_than_required():
     large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
     assert large_batch.can_allocate(small_line)
+
 
 def test_cannot_allocate_if_available_smaller_than_required():
     small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
     assert small_batch.can_allocate(large_line) is False
 
+
 def test_can_allocate_if_available_equal_to_required():
     batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
     assert batch.can_allocate(line)
+
 
 def test_cannot_allocate_if_skus_do_not_match():
     batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)

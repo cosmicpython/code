0c4c27ec6a70840099d099d77d33b8ce1142b2bd
Harry <hjwp2@cantab.net>
2019-03-11 00:48:49 +0000

    ORM for _allocations set on Batch

--- orm.py
+++ orm.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String, Date
-from sqlalchemy.orm import mapper
+from sqlalchemy import (
+    Table, MetaData, Column, Integer, String, Date,
+    ForeignKey
+)
+from sqlalchemy.orm import mapper, relationship
 
 import model
 
@@ -8,20 +11,35 @@ metadata = MetaData()
 
 order_lines = Table(
     'order_lines', metadata,
-    Column('orderid', String(255), primary_key=True),
-    Column('sku', String(255), primary_key=True),
-    Column('qty', Integer),
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('sku', String(255)),
+    Column('qty', Integer, nullable=False),
+    Column('orderid', String(255)),
 )
 
 batches = Table(
     'batches', metadata,
-    Column('reference', String(255), primary_key=True),
-    Column('sku', String(255), primary_key=True),
-    Column('_purchased_qty', Integer),
-    Column('eta', Date),
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('reference', String(255)),
+    Column('sku', String(255)),
+    Column('_purchased_quantity', Integer, nullable=False),
+    Column('eta', Date, nullable=True),
+)
+
+allocations = Table(
+    'allocations', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('orderline_id', ForeignKey('order_lines.id')),
+    Column('batch_id', ForeignKey('batches.id')),
 )
 
 
 def start_mappers():
-    mapper(model.OrderLine, order_lines)
-    mapper(model.Batch, batches)
+    lines_mapper = mapper(model.OrderLine, order_lines)
+    mapper(model.Batch, batches, properties={
+        '_allocations': relationship(
+            lines_mapper,
+            secondary=allocations,
+            collection_class=set,
+        )
+    })

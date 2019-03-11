b635a154d8ddf58a3bbf40a1d5c96c65430bc49d
Harry <hjwp2@cantab.net>
2019-03-11 01:10:39 +0000

    repository for batches [chapter_02_repository_ends]

---
+++ repository.py
@@ -0,0 +1,26 @@
+import abc
+import model
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> model.Batch:
+        raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(model.Batch).all()

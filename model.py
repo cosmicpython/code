b91ab8912511efe497e6cf434c0e7ee84a05f963
Harry <hjwp2@cantab.net>
2019-03-11 15:37:37 +0000

    raising out of stock exception [out_of_stock]

--- model.py
+++ model.py
@@ -4,12 +4,19 @@ from datetime import date
 from typing import Optional, List, Set
 
 
+class OutOfStock(Exception):
+    pass
+
+
 def allocate(line: OrderLine, batches: List[Batch]) -> str:
-    batch = next(
-        b for b in sorted(batches) if b.can_allocate(line)
-    )
-    batch.allocate(line)
-    return batch.reference
+    try:
+        batch = next(
+            b for b in sorted(batches) if b.can_allocate(line)
+        )
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f'Out of stock for sku {line.sku}')
 
 
 @dataclass(frozen=True)

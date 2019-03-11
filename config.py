fda077db691e09e69eb54e7fbf29feb732da37b5
Harry <hjwp2@cantab.net>
2019-03-11 04:10:52 +0000

    first api tests [first_api_test]

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+
+def get_postgres_uri():
+    host = os.environ.get("DB_HOST", "localhost")
+    port = 54321 if host == "localhost" else 5432
+    password = os.environ.get("DB_PASSWORD", "abc123")
+    user, db_name = "allocation", "allocation"
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+def get_api_url():
+    host = os.environ.get("API_HOST", "localhost")
+    port = 5005 if host == "localhost" else 80
+    return f"http://{host}:{port}"
---
+++ test_api.py
@@ -0,0 +1,43 @@
+import uuid
+import pytest
+import requests
+
+import config
+
+
+def random_suffix():
+    return uuid.uuid4().hex[:6]
+
+
+def random_sku(name=""):
+    return f"sku-{name}-{random_suffix()}"
+
+
+def random_batchref(name=""):
+    return f"batch-{name}-{random_suffix()}"
+
+
+def random_orderid(name=""):
+    return f"order-{name}-{random_suffix()}"
+
+
+@pytest.mark.usefixtures("restart_api")
+def test_api_returns_allocation(add_stock):
+    sku, othersku = random_sku(), random_sku("other")
+    earlybatch = random_batchref(1)
+    laterbatch = random_batchref(2)
+    otherbatch = random_batchref(3)
+    add_stock(
+        [
+            (laterbatch, sku, 100, "2011-01-02"),
+            (earlybatch, sku, 100, "2011-01-01"),
+            (otherbatch, othersku, 100, None),
+        ]
+    )
+    data = {"orderid": random_orderid(), "sku": sku, "qty": 3}
+    url = config.get_api_url()
+
+    r = requests.post(f"{url}/allocate", json=data)
+
+    assert r.status_code == 201
+    assert r.json()["batchref"] == earlybatch

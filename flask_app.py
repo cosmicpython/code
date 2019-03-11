8fc086ef836fd5fcf726d37e285e0e9d4ace49f4
Harry <hjwp2@cantab.net>
2019-03-11 04:21:51 +0000

    first cut of flask app [first_cut_flask_app]

---
+++ flask_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+import model
+import orm
+import repository
+
+
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
+app = Flask(__name__)
+
+
+@app.route("/allocate", methods=["POST"])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json["orderid"], request.json["sku"], request.json["qty"],
+    )
+
+    batchref = model.allocate(line, batches)
+
+    return {"batchref": batchref}, 201

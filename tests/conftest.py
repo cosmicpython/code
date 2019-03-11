a9300576edd630a24dff73a3c733b4d5de7ba2d7
Harry <hjwp2@cantab.net>
2019-03-11 23:11:44 +0000

    basic uow test, uow and conftest.py changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,10 +21,15 @@ def in_memory_db():
 
 
 @pytest.fixture
-def session(in_memory_db):
+def session_factory(in_memory_db):
     start_mappers()
-    yield sessionmaker(bind=in_memory_db)()
+    yield sessionmaker(bind=in_memory_db)
     clear_mappers()
+
+
+@pytest.fixture
+def session(session_factory):
+    return session_factory()
 
 
 def wait_for_postgres_to_come_up(engine):

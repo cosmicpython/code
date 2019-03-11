0808c3591133e39eeb73b6a0e40af4a0c56cb18c
Harry <hjwp2@cantab.net>
2019-03-11 23:11:44 +0000

    basic uow test, uow and conftest.py changes

---
+++ src/allocation/unit_of_work.py
@@ -0,0 +1,51 @@
+# pylint: disable=attribute-defined-outside-init
+import abc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from allocation import config
+from allocation import repository
+
+
+class AbstractUnitOfWork(abc.ABC):
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @abc.abstractmethod
+    def commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
+
+    def init_repositories(self, batches: repository.AbstractRepository):
+        self._batches = batches
+
+    @property
+    def batches(self) -> repository.AbstractRepository:
+        return self._batches
+
+
+
+DEFAULT_SESSION_FACTORY = sessionmaker(bind=create_engine(
+    config.get_postgres_uri(),
+))
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session = session_factory()  # type: Session
+        self.init_repositories(repository.SqlAlchemyRepository(self.session))
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
+

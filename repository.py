7af6ba6d38057b5cfd96b8e512f4d4dfc90786c2
Harry <hjwp2@cantab.net>
2019-03-11 01:10:39 +0000

    repository for batches [chapter_02_repository_ends]

---
+++ repository.py
@@ -0,0 +1,15 @@
+import model
+
+class BatchRepository:
+
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(model.Batch).all()

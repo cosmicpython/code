1b8467b125604fca3bca218fa48589bc219a7ab3
Harry <hjwp2@cantab.net>
2019-03-11 00:48:49 +0000

    ORM for _allocations set on Batch

--- test_orm.py
+++ test_orm.py
@@ -26,9 +26,15 @@ def test_orderline_mapper_can_save_lines(session):
 
 
 
-def test_batches(session):
-    session.execute('INSERT INTO "batches" VALUES ("batch1", "sku1", 100, null)')
-    session.execute('INSERT INTO "batches" VALUES ("batch2", "sku2", 200, "2011-04-11")')
+def test_retrieving_batches(session):
+    session.execute(
+        'INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
+        ' VALUES ("batch1", "sku1", 100, null)'
+    )
+    session.execute(
+        'INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
+        ' VALUES ("batch2", "sku2", 200, "2011-04-11")'
+    )
     expected = [
         model.Batch("batch1", "sku1", 100, eta=None),
         model.Batch("batch2", "sku2", 200, eta=date(2011, 4, 11)),
@@ -36,3 +42,49 @@ def test_batches(session):
 
     assert session.query(model.Batch).all() == expected
 
+
+def test_saving_batches(session):
+    batch = model.Batch('batch1', 'sku1', 100, eta=None)
+    session.add(batch)
+    session.commit()
+    rows = list(session.execute(
+        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
+    ))
+    assert rows == [('batch1', 'sku1', 100, None)]
+
+def test_saving_allocations(session):
+    batch = model.Batch('batch1', 'sku1', 100, eta=None)
+    line = model.OrderLine('order1', 'sku1', 10)
+    batch.allocate(line)
+    session.add(batch)
+    session.commit()
+    rows = list(session.execute('SELECT orderline_id, batch_id FROM "allocations"'))
+    assert rows == [(batch.id, line.id)]
+
+
+def test_retrieving_allocations(session):
+    session.execute(
+        'INSERT INTO order_lines (orderid, sku, qty) VALUES ("order1", "sku1", 12)'
+    )
+    [[olid]] = session.execute(
+        'SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku',
+        dict(orderid='order1', sku='sku1')
+    )
+    session.execute(
+        'INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
+        ' VALUES ("batch1", "sku1", 100, null)'
+    )
+    [[bid]] = session.execute(
+        'SELECT id FROM batches WHERE reference=:ref AND sku=:sku',
+        dict(ref='batch1', sku='sku1')
+    )
+    session.execute(
+        'INSERT INTO allocations (orderline_id, batch_id) VALUES (:olid, :bid)',
+        dict(olid=olid, bid=bid)
+    )
+
+    batch = session.query(model.Batch).one()
+
+    assert batch._allocations == {
+        model.OrderLine("order1", "sku1", 12)
+    }

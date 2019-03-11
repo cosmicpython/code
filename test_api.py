9fa305e0b9b88aca731a7233813ec4f83fadaacf
Harry <hjwp2@cantab.net>
2019-03-11 04:10:52 +0000

    first api tests [first_api_test]

---
+++ test_api.py
@@ -0,0 +1,33 @@
+import uuid
+import pytest
+import requests
+
+import config
+
+def random_suffix():
+    return uuid.uuid4().hex[:6]
+
+def random_sku(name=''):
+    return f'sku-{name}-{random_suffix()}'
+
+def random_batchref(name=''):
+    return f'batch-{name}-{random_suffix()}'
+
+def random_orderid(name=''):
+    return f'order-{name}-{random_suffix()}'
+
+
+@pytest.mark.usefixtures('restart_api')
+def test_api_returns_allocation(add_stock):
+    sku, othersku = random_sku(), random_sku('other')
+    batch1, batch2, batch3 = random_batchref(1), random_batchref(2), random_batchref(3)
+    add_stock([
+        (batch1, sku, 100, '2011-01-02'),
+        (batch2, sku, 100, '2011-01-01'),
+        (batch3, othersku, 100, None),
+    ])
+    data = {'orderid': random_orderid(), 'sku': sku, 'qty': 3}
+    url = config.get_api_url()
+    r = requests.post(f'{url}/allocate', json=data)
+    assert r.status_code == 201
+    assert r.json()['batchref'] == batch2

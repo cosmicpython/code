7959ae6df225489eb3a08d6ff440e32727518195
Harry <hjwp2@cantab.net>
2019-03-12 11:52:13 +0000

    two more tests for rollback behaviour [chapter_06_uow_ends]

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -1,5 +1,7 @@
+import pytest
 from allocation.domain import model
 from allocation.service_layer import unit_of_work
+
 
 def insert_batch(session, ref, sku, qty, eta):
     session.execute(
@@ -7,6 +9,7 @@ def insert_batch(session, ref, sku, qty, eta):
         ' VALUES (:ref, :sku, :qty, :eta)',
         dict(ref=ref, sku=sku, qty=qty, eta=eta)
     )
+
 
 def get_allocated_batch_ref(session, orderid, sku):
     [[orderlineid]] = session.execute(
@@ -35,3 +38,28 @@ def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
 
     batchref = get_allocated_batch_ref(session, 'o1', 'HIPSTER-WORKBENCH')
     assert batchref == 'batch1'
+
+
+def test_rolls_back_uncommitted_work_by_default(session_factory):
+    uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
+    with uow:
+        insert_batch(uow.session, 'batch1', 'MEDIUM-PLINTH', 100, None)
+
+    new_session = session_factory()
+    rows = list(new_session.execute('SELECT * FROM "batches"'))
+    assert rows == []
+
+
+def test_rolls_back_on_error(session_factory):
+    class MyException(Exception):
+        pass
+
+    uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
+    with pytest.raises(MyException):
+        with uow:
+            insert_batch(uow.session, 'batch1', 'LARGE-FORK', 100, None)
+            raise MyException()
+
+    new_session = session_factory()
+    rows = list(new_session.execute('SELECT * FROM "batches"'))
+    assert rows == []

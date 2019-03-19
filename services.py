d1e2e6e59fb8fe4020bec8425bdbd30b9648ff08
Harry <hjwp2@cantab.net>
2019-03-19 13:44:44 +0000

    services layer with valid-sku check [service_function]

---
+++ services.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import model
+from model import OrderLine
+from repository import AbstractRepository
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
+
+def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+    batches = repo.list()
+    if not is_valid_sku(line.sku, batches):
+        raise InvalidSku(f"Invalid sku {line.sku}")
+    batchref = model.allocate(line, batches)
+    session.commit()
+    return batchref

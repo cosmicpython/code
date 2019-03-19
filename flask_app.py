80dece9d1736a3abf2ae605af25606c85b39bccb
Harry <hjwp2@cantab.net>
2019-03-19 12:17:15 +0000

    flask now does error handling [flask_error_handling]

--- flask_app.py
+++ flask_app.py
@@ -13,6 +13,10 @@ get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
 
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
+
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
     session = get_session()
@@ -21,7 +25,13 @@ def allocate_endpoint():
         request.json["orderid"], request.json["sku"], request.json["qty"],
     )
 
-    batchref = model.allocate(line, batches)
+    if not is_valid_sku(line.sku, batches):
+        return {"message": f"Invalid sku {line.sku}"}, 400
+
+    try:
+        batchref = model.allocate(line, batches)
+    except model.OutOfStock as e:
+        return {"message": str(e)}, 400
 
     session.commit()
     return {"batchref": batchref}, 201

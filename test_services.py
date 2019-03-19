a2c2332ee01fdefa44569b27ef7db7200c90e8d2
Harry <hjwp2@cantab.net>
2019-03-19 13:44:27 +0000

    first tests for the services layer [first_services_tests]

---
+++ test_services.py
@@ -0,0 +1,22 @@
+import pytest
+
+import model
+import services
+
+
+def test_returns_allocation():
+    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
+    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    result = services.allocate(line, repo, FakeSession())
+    assert result == "b1"
+
+
+def test_error_for_invalid_sku():
+    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
+    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate(line, repo, FakeSession())

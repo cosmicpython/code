213243a02dd7d0ac29698bd8199c6fd9c7c302db
Harry <hjwp2@cantab.net>
2019-03-19 12:10:34 +0000

    test some 400 error cases [test_error_cases]

--- test_api.py
+++ test_api.py
@@ -57,3 +57,26 @@ def test_allocations_are_persisted(add_stock):
     r = requests.post(f'{url}/allocate', json=line2)
     assert r.status_code == 201
     assert r.json()['batchref'] == batch2
+
+
+@pytest.mark.usefixtures('restart_api')
+def test_400_message_for_out_of_stock(add_stock):
+    sku, smalL_batch, large_order = random_sku(), random_batchref(), random_orderid()
+    add_stock([
+        (smalL_batch, sku, 10, '2011-01-01'),
+    ])
+    data = {'orderid': large_order, 'sku': sku, 'qty': 20}
+    url = config.get_api_url()
+    r = requests.post(f'{url}/allocate', json=data)
+    assert r.status_code == 400
+    assert r.json()['message'] == f'Out of stock for sku {sku}'
+
+
+@pytest.mark.usefixtures('restart_api')
+def test_400_message_for_invalid_sku():
+    unknown_sku, orderid = random_sku(), random_orderid()
+    data = {'orderid': orderid, 'sku': unknown_sku, 'qty': 20}
+    url = config.get_api_url()
+    r = requests.post(f'{url}/allocate', json=data)
+    assert r.status_code == 400
+    assert r.json()['message'] == f'Invalid sku {unknown_sku}'

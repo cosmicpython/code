39ee0588fc26aec3e518b760024e7beff5a6804e
Harry <hjwp2@cantab.net>
2019-03-19 11:58:59 +0000

    test persistence by double-allocating. [second_api_test]

--- test_api.py
+++ test_api.py
@@ -25,3 +25,24 @@ def test_api_returns_allocation(add_stock):
     r = requests.post(f'{url}/allocate', json=data)
     assert r.status_code == 201
     assert r.json()['batchid'] == batch2
+
+
+@pytest.mark.usefixtures('restart_api')
+def test_allocations_are_persisted(add_stock):
+    sku = random_ref('s1')
+    batch1, batch2 = random_ref('b1'), random_ref('b2')
+    order1, order2 = random_ref('o1'), random_ref('o2')
+    add_stock([
+        (batch1, sku, 10, '2011-01-01'),
+        (batch2, sku, 10, '2011-01-02'),
+    ])
+    line1 = {'orderid': order1, 'sku': sku, 'qty': 10}
+    line2 = {'orderid': order2, 'sku': sku, 'qty': 10}
+    url = config.get_api_url()
+    r = requests.post(f'{url}/allocate', json=line1)
+    assert r.status_code == 201
+    assert r.json()['batchid'] == batch1
+    r = requests.post(f'{url}/allocate', json=line2)
+    assert r.status_code == 201
+    assert r.json()['batchid'] == batch2
+

986f8491e186373de690c80a0c244ec9a35f3867
Harry <hjwp2@cantab.net>
2019-03-19 14:33:19 +0000

    modify flask app to use service layer

--- flask_app.py
+++ flask_app.py
@@ -6,33 +6,26 @@ import config
 import model
 import orm
 import repository
+import services
 
 
 orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
-def is_valid_sku(sku, batches):
-    return sku in {b.sku for b in batches}
-
 @app.route("/allocate", methods=['POST'])
 def allocate_endpoint():
     session = get_session()
-    batches = repository.BatchRepository(session).list()
+    repo = repository.BatchRepository(session)
     line = model.OrderLine(
         request.json['orderid'],
         request.json['sku'],
         request.json['qty'],
     )
-
-    if not is_valid_sku(line.sku, batches):
-        return jsonify({'message': f'Invalid sku {line.sku}'}), 400
-
     try:
-        batchid = model.allocate(line, batches)
-    except model.OutOfStock as e:
+        batchid = services.allocate(line, repo, session)
+    except (model.OutOfStock, services.InvalidSku) as e:
         return jsonify({'message': str(e)}), 400
 
-    session.commit()
     return jsonify({'batchid': batchid}), 201
 

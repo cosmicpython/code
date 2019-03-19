b724e267b792f4d77982a39e6f1935f0fc465ec5
Harry <hjwp2@cantab.net>
2019-03-19 14:48:36 +0000

    strip out unecessary tests from e2e layer [fewer_e2e_tests]

--- test_api.py
+++ test_api.py
@@ -22,7 +22,7 @@ def random_orderid(name=""):
 
 
 @pytest.mark.usefixtures("restart_api")
-def test_api_returns_allocation(add_stock):
+def test_happy_path_returns_201_and_allocated_batch(add_stock):
     sku, othersku = random_sku(), random_sku("other")
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
@@ -44,43 +44,7 @@ def test_api_returns_allocation(add_stock):
 
 
 @pytest.mark.usefixtures("restart_api")
-def test_allocations_are_persisted(add_stock):
-    sku = random_sku()
-    batch1, batch2 = random_batchref(1), random_batchref(2)
-    order1, order2 = random_orderid(1), random_orderid(2)
-    add_stock(
-        [(batch1, sku, 10, "2011-01-01"), (batch2, sku, 10, "2011-01-02"),]
-    )
-    line1 = {"orderid": order1, "sku": sku, "qty": 10}
-    line2 = {"orderid": order2, "sku": sku, "qty": 10}
-    url = config.get_api_url()
-
-    # first order uses up all stock in batch 1
-    r = requests.post(f"{url}/allocate", json=line1)
-    assert r.status_code == 201
-    assert r.json()["batchref"] == batch1
-
-    # second order should go to batch 2
-    r = requests.post(f"{url}/allocate", json=line2)
-    assert r.status_code == 201
-    assert r.json()["batchref"] == batch2
-
-
-@pytest.mark.usefixtures("restart_api")
-def test_400_message_for_out_of_stock(add_stock):
-    sku, small_batch, large_order = random_sku(), random_batchref(), random_orderid()
-    add_stock(
-        [(small_batch, sku, 10, "2011-01-01"),]
-    )
-    data = {"orderid": large_order, "sku": sku, "qty": 20}
-    url = config.get_api_url()
-    r = requests.post(f"{url}/allocate", json=data)
-    assert r.status_code == 400
-    assert r.json()["message"] == f"Out of stock for sku {sku}"
-
-
-@pytest.mark.usefixtures("restart_api")
-def test_400_message_for_invalid_sku():
+def test_unhappy_path_returns_400_and_error_message():
     unknown_sku, orderid = random_sku(), random_orderid()
     data = {"orderid": orderid, "sku": unknown_sku, "qty": 20}
     url = config.get_api_url()

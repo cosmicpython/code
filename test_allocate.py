eb49ab43e97975b01ca96610d943082eb38bd2b4
Harry <hjwp2@cantab.net>
2019-03-20 14:26:35 +0000

    change tests add one for return

--- test_allocate.py
+++ test_allocate.py
@@ -27,3 +27,11 @@ def test_prefers_earlier_batches():
     assert earliest.available_quantity == 90
     assert medium.available_quantity == 100
     assert latest.available_quantity == 100
+
+
+def test_returns_allocated_batch_ref():
+    in_stock_batch = Batch("in-stock-batch-ref", "HIGHBROW-POSTER", 100, eta=None)
+    shipment_batch = Batch("shipment-batch-ref", "HIGHBROW-POSTER", 100, eta=tomorrow)
+    line = OrderLine("oref", "HIGHBROW-POSTER", 10)
+    allocation = allocate(line, [in_stock_batch, shipment_batch])
+    assert allocation == "in-stock-batch"

0e1b8068ffa687140447a4e1d4510dc9a628a408
Harry <hjwp2@cantab.net>
2019-03-20 10:53:14 +0000

    use uow in services, flask app

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,42 +1,34 @@
 from datetime import datetime
 from flask import Flask, jsonify, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-from allocation import config
 from allocation.domain import model
-from allocation.adapters import orm, repository
-from allocation.service_layer import services
+from allocation.adapters import orm
+from allocation.service_layer import services, unit_of_work
 
-orm.start_mappers()
-get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
+orm.start_mappers()
 
 
 @app.route("/add_batch", methods=['POST'])
 def add_batch():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
     eta = request.json['eta']
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
     services.add_batch(
         request.json['ref'], request.json['sku'], request.json['qty'], eta,
-        repo, session
+        unit_of_work.SqlAlchemyUnitOfWork(),
     )
     return 'OK', 201
 
 
 @app.route("/allocate", methods=['POST'])
 def allocate_endpoint():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
     try:
         batchref = services.allocate(
             request.json['orderid'],
             request.json['sku'],
             request.json['qty'],
-            repo, session
+            unit_of_work.SqlAlchemyUnitOfWork(),
         )
     except (model.OutOfStock, services.InvalidSku) as e:
         return jsonify({'message': str(e)}), 400
--- src/allocation/service_layer/services.py
+++ src/allocation/service_layer/services.py
@@ -4,7 +4,8 @@ from datetime import date
 
 from allocation.domain import model
 from allocation.domain.model import OrderLine
-from allocation.adapters.repository import AbstractRepository
+from allocation.service_layer import unit_of_work
+
 
 class InvalidSku(Exception):
     pass
@@ -16,19 +17,22 @@ def is_valid_sku(sku, batches):
 
 def add_batch(
         ref: str, sku: str, qty: int, eta: Optional[date],
-        repo: AbstractRepository, session,
+        uow: unit_of_work.AbstractUnitOfWork
 ):
-    repo.add(model.Batch(ref, sku, qty, eta))
-    session.commit()
+    with uow:
+        uow.batches.add(model.Batch(ref, sku, qty, eta))
+        uow.commit()
 
 
 def allocate(
-        orderid: str, sku: str, qty: int, repo: AbstractRepository, session
+        orderid: str, sku: str, qty: int,
+        uow: unit_of_work.AbstractUnitOfWork
 ) -> str:
     line = OrderLine(orderid, sku, qty)
-    batches = repo.list()
-    if not is_valid_sku(line.sku, batches):
-        raise InvalidSku(f'Invalid sku {line.sku}')
-    batchref = model.allocate(line, batches)
-    session.commit()
+    with uow:
+        batches = uow.batches.list()
+        if not is_valid_sku(line.sku, batches):
+            raise InvalidSku(f'Invalid sku {line.sku}')
+        batchref = model.allocate(line, batches)
+        uow.commit()
     return batchref

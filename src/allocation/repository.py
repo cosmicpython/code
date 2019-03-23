bad0e5b3e456a2a3366c380edc9ca06b9f9cf392
Harry <hjwp2@cantab.net>
2019-03-23 23:40:06 +0000

    select for update is one approach [with_for_update]

--- src/allocation/repository.py
+++ src/allocation/repository.py
@@ -9,6 +9,8 @@ class ProductRepository:
         self.session.add(product)
 
     def get(self, sku):
-        print(sku, type(sku))
-        return self.session.query(model.Product).filter_by(sku=sku).first()
+        return self.session.query(model.Product) \
+                           .filter_by(sku=sku) \
+                           .with_for_update() \
+                           .first()
 

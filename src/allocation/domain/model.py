48cb8b984bc72f52989a4a87da21427cc72c3e47
Harry <hjwp2@cantab.net>
2019-03-23 22:59:22 +0000

    first cut of a product with a version number

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -9,14 +9,16 @@ class OutOfStock(Exception):
 
 
 class Product:
-    def __init__(self, sku: str, batches: List[Batch]):
+    def __init__(self, sku: str, batches: List[Batch], version_number: int = 0):
         self.sku = sku
         self.batches = batches
+        self.version_number = version_number
 
     def allocate(self, line: OrderLine) -> str:
         try:
             batch = next(b for b in sorted(self.batches) if b.can_allocate(line))
             batch.allocate(line)
+            self.version_number += 1
             return batch.reference
         except StopIteration:
             raise OutOfStock(f"Out of stock for sku {line.sku}")

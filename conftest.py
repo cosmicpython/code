dd2e8b94cea96730db65e1a291302bab1aa23a93
Harry <hjwp2@cantab.net>
2019-03-26 21:21:17 +0000

    fix conftest waits, requirements.txt and travis config

--- conftest.py
+++ conftest.py
@@ -3,11 +3,14 @@ import time
 from pathlib import Path
 
 import pytest
+import requests
+from requests.exceptions import ConnectionError
+from sqlalchemy.exc import OperationalError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 
 from orm import metadata, start_mappers
-from config import get_postgres_uri
+import config
 
 
 @pytest.fixture
@@ -24,9 +27,32 @@ def session(in_memory_db):
     clear_mappers()
 
 
+def wait_for_postgres_to_come_up(engine):
+    deadline = time.time() + 10
+    while time.time() < deadline:
+        try:
+            return engine.connect()
+        except OperationalError:
+            time.sleep(0.5)
+    pytest.fail('Postgres never came up')
+
+
+def wait_for_webapp_to_come_up():
+    deadline = time.time() + 10
+    url = config.get_api_url()
+    while time.time() < deadline:
+        try:
+            return requests.get(url)
+        except ConnectionError:
+            time.sleep(0.5)
+    pytest.fail('API never came up')
+
+
+
 @pytest.fixture(scope='session')
 def postgres_db():
-    engine = create_engine(get_postgres_uri())
+    engine = create_engine(config.get_postgres_uri())
+    wait_for_postgres_to_come_up(engine)
     metadata.create_all(engine)
     return engine
 
@@ -80,5 +106,6 @@ def add_stock(postgres_session):
 @pytest.fixture
 def restart_api():
     (Path(__file__).parent / 'flask_app.py').touch()
-    time.sleep(0.3)
+    time.sleep(0.5)
+    wait_for_webapp_to_come_up()
 

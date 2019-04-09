1a3b48d2b352ea6e2270e368ad62d446a91f8563
Harry <hjwp2@cantab.net>
2019-04-09 11:23:24 +0100

    putting it in the services layer isn't lovely either [email_in_services]

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from dataclasses import dataclass
 from datetime import date
 from typing import Optional, List, Set
-from allocation.adapters import email
 
 
 class OutOfStock(Exception):
@@ -22,7 +21,6 @@ class Product:
             self.version_number += 1
             return batch.reference
         except StopIteration:
-            email.send_mail("stock@made.com", f"Out of stock for {line.sku}")
             raise OutOfStock(f"Out of stock for sku {line.sku}")
 
 

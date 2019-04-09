5a5e07c681516a37b478416c35b8b0dd1cc2964e
Harry <hjwp2@cantab.net>
2019-04-09 13:07:55 +0100

    a little hack in the orm so that events work

--- src/allocation/adapters/orm.py
+++ src/allocation/adapters/orm.py
@@ -1,4 +1,13 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
+from sqlalchemy import (
+    Table,
+    MetaData,
+    Column,
+    Integer,
+    String,
+    Date,
+    ForeignKey,
+    event,
+)
 from sqlalchemy.orm import mapper, relationship
 
 from allocation.domain import model
@@ -55,5 +64,12 @@ def start_mappers():
         },
     )
     mapper(
-        model.Product, products, properties={"batches": relationship(batches_mapper)}
+        model.Product,
+        products,
+        properties={"batches": relationship(batches_mapper)},
     )
+
+
+@event.listens_for(model.Product, "load")
+def receive_load(product, _):
+    product.events = []

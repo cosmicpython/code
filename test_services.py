eded7ebf6cc5f0fb05dc547af6daee6549e171ca
Harry <hjwp2@cantab.net>
2019-04-23 12:52:08 +0100

    test commmits [second_services_test]

--- test_services.py
+++ test_services.py
@@ -41,3 +41,13 @@ def test_error_for_invalid_sku():
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
         services.allocate(line, repo, FakeSession())
+
+
+def test_commits():
+    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
+    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
+    repo = FakeRepository([batch])
+    session = FakeSession()
+
+    services.allocate(line, repo, session)
+    assert session.committed is True

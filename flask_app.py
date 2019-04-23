d186455747a1cc93f0a40b28655c858ab55ddb61
Harry <hjwp2@cantab.net>
2019-04-23 12:44:09 +0100

    modify flask app to use new service layer api [api_uses_modified_service]

--- flask_app.py
+++ flask_app.py
@@ -17,13 +17,13 @@ app = Flask(__name__)
 def allocate_endpoint():
     session = get_session()
     repo = repository.SqlAlchemyRepository(session)
-    line = model.OrderLine(
-        request.json['orderid'],
-        request.json['sku'],
-        request.json['qty'],
-    )
     try:
-        batchref = services.allocate(line, repo, session)
+        batchref = services.allocate(
+            request.json['orderid'],
+            request.json['sku'],
+            request.json['qty'],
+            repo, session
+        )
     except (model.OutOfStock, services.InvalidSku) as e:
         return jsonify({'message': str(e)}), 400
 

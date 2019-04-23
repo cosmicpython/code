fea885aa6d8af5df72b86126c864a95b5f5b8aba
Harry <hjwp2@cantab.net>
2019-04-23 12:51:43 +0100

    fake repository and fake session [fake_repo]

--- test_services.py
+++ test_services.py
@@ -1,7 +1,29 @@
 import pytest
-
 import model
+import repository
 import services
+
+
+class FakeRepository(repository.AbstractRepository):
+
+    def __init__(self, batches):
+        self._batches = set(batches)
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return list(self._batches)
+
+
+class FakeSession():
+    committed = False
+
+    def commit(self):
+        self.committed = True
 
 
 def test_returns_allocation():

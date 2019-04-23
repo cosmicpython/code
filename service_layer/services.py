8766fe93c6345ea812410c5c6ba9974f44f1f04c
Harry <hjwp2@cantab.net>
2019-04-23 12:11:47 +0100

    rewrite service layer to take primitives [service_takes_primitives]

--- service_layer/services.py
+++ service_layer/services.py
@@ -11,7 +11,10 @@ class InvalidSku(Exception):
 def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+def allocate(
+        orderid: str, sku: str, qty: int, repo: AbstractRepository, session
+) -> str:
+    line = OrderLine(orderid, sku, qty)
     batches = repo.list()
     if not is_valid_sku(line.sku, batches):
         raise InvalidSku(f'Invalid sku {line.sku}')

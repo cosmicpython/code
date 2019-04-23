308c638c25bc8fde2ef028f33e9a78d785e2139b
Harry <hjwp2@cantab.net>
2019-04-23 12:59:52 +0100

    fixture function for batches [services_factory_function]

--- test_services.py
+++ test_services.py
@@ -6,6 +6,12 @@ import services
 
 class FakeRepository(repository.AbstractRepository):
 
+    @staticmethod
+    def for_batch(ref, sku, qty, eta=None):
+        return FakeRepository([
+            model.Batch(ref, sku, qty, eta),
+        ])
+
     def __init__(self, batches):
         self._batches = set(batches)
 
@@ -27,25 +33,19 @@ class FakeSession():
 
 
 def test_returns_allocation():
-    batch = model.Batch("batch1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
-
+    repo = FakeRepository.for_batch("batch1", "COMPLICATED-LAMP", 100, eta=None)
     result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, FakeSession())
     assert result == "batch1"
 
 
 def test_error_for_invalid_sku():
-    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
-    repo = FakeRepository([batch])
-
+    repo = FakeRepository.for_batch('b1', 'AREALSKU', 100, eta=None)
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
         services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
 
 
 def test_commits():
-    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
-    repo = FakeRepository([batch])
+    repo = FakeRepository.for_batch("b1", "OMINOUS-MIRROR", 100, eta=None)
     session = FakeSession()
-
     services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
     assert session.committed is True

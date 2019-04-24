c76dcecd099c6e1296f8cb2e2049e91d55ec1044
Harry <hjwp2@cantab.net>
2019-04-24 12:21:59 +0100

    add api endpoint for add_batch [api_for_add_batch]

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, jsonify, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,10 +9,24 @@ import orm
 import repository
 import services
 
-
 orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
+
+
+@app.route("/add_batch", methods=['POST'])
+def add_batch():
+    session = get_session()
+    repo = repository.SqlAlchemyRepository(session)
+    eta = request.json['eta']
+    if eta is not None:
+        eta = datetime.fromisoformat(eta).date()
+    services.add_batch(
+        request.json['ref'], request.json['sku'], request.json['qty'], eta,
+        repo, session
+    )
+    return 'OK', 201
+
 
 @app.route("/allocate", methods=['POST'])
 def allocate_endpoint():

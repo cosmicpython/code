4338f3e97e799c2504ab83756d39e224550a1b1b
Harry <hjwp2@cantab.net>
2019-04-24 12:22:34 +0100

    api tests no longer need hardcoded sql fixture [api_tests_with_no_sql]

--- conftest.py
+++ conftest.py
@@ -65,45 +65,6 @@ def postgres_session(postgres_db):
 
 
 @pytest.fixture
-def add_stock(postgres_session):
-    batches_added = set()
-    skus_added = set()
-
-    def _add_stock(lines):
-        for ref, sku, qty, eta in lines:
-            postgres_session.execute(
-                'INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
-                ' VALUES (:ref, :sku, :qty, :eta)',
-                dict(ref=ref, sku=sku, qty=qty, eta=eta),
-            )
-            [[batch_id]] = postgres_session.execute(
-                'SELECT id FROM batches WHERE reference=:ref AND sku=:sku',
-                dict(ref=ref, sku=sku),
-            )
-            batches_added.add(batch_id)
-            skus_added.add(sku)
-        postgres_session.commit()
-
-    yield _add_stock
-
-    for batch_id in batches_added:
-        postgres_session.execute(
-            'DELETE FROM allocations WHERE batch_id=:batch_id',
-            dict(batch_id=batch_id),
-        )
-        postgres_session.execute(
-            'DELETE FROM batches WHERE id=:batch_id',
-            dict(batch_id=batch_id),
-        )
-    for sku in skus_added:
-        postgres_session.execute(
-            'DELETE FROM order_lines WHERE sku=:sku',
-            dict(sku=sku),
-        )
-        postgres_session.commit()
-
-
-@pytest.fixture
 def restart_api():
     (Path(__file__).parent / 'flask_app.py').touch()
     time.sleep(0.5)

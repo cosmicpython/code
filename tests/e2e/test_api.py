9a4d80f29e86560f3b7056fc0fff80f1e1ff8af7
Harry <hjwp2@cantab.net>
2019-04-24 12:22:34 +0100

    api tests no longer need hardcoded sql fixture [chapter_05_high_gear_low_gear_ends]

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -17,17 +17,25 @@ def random_orderid(name=''):
     return f'order-{name}-{random_suffix()}'
 
 
+def post_to_add_batch(ref, sku, qty, eta):
+    url = config.get_api_url()
+    r = requests.post(
+        f'{url}/add_batch',
+        json={'ref': ref, 'sku': sku, 'qty': qty, 'eta': eta}
+    )
+    assert r.status_code == 201
+
+
+@pytest.mark.usefixtures('postgres_db')
 @pytest.mark.usefixtures('restart_api')
-def test_happy_path_returns_201_and_allocated_batch(add_stock):
+def test_happy_path_returns_201_and_allocated_batch():
     sku, othersku = random_sku(), random_sku('other')
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
     otherbatch = random_batchref(3)
-    add_stock([
-        (laterbatch, sku, 100, '2011-01-02'),
-        (earlybatch, sku, 100, '2011-01-01'),
-        (otherbatch, othersku, 100, None),
-    ])
+    post_to_add_batch(laterbatch, sku, 100, '2011-01-02')
+    post_to_add_batch(earlybatch, sku, 100, '2011-01-01')
+    post_to_add_batch(otherbatch, othersku, 100, None)
     data = {'orderid': random_orderid(), 'sku': sku, 'qty': 3}
     url = config.get_api_url()
     r = requests.post(f'{url}/allocate', json=data)
@@ -35,6 +43,7 @@ def test_happy_path_returns_201_and_allocated_batch(add_stock):
     assert r.json()['batchref'] == earlybatch
 
 
+@pytest.mark.usefixtures('postgres_db')
 @pytest.mark.usefixtures('restart_api')
 def test_unhappy_path_returns_400_and_error_message():
     unknown_sku, orderid = random_sku(), random_orderid()

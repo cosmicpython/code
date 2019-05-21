c9e6370a2aa22fc23e16f64969a289293e760cc2
Harry <hjwp2@cantab.net>
2019-05-21 12:14:14 +0100

    some e2e tests [ugly_sync_tests]

---
+++ test_sync.py
@@ -0,0 +1,45 @@
+import tempfile
+from pathlib import Path
+import shutil
+from sync import sync
+
+
+def test_when_a_file_exists_in_the_source_but_not_the_destination():
+    try:
+        source = tempfile.mkdtemp()
+        dest = tempfile.mkdtemp()
+
+        content = "I am a very useful file"
+        (Path(source) / "my-file").write_text(content)
+
+        sync(source, dest)
+
+        expected_path = Path(dest) / "my-file"
+        assert expected_path.exists()
+        assert expected_path.read_text() == content
+
+    finally:
+        shutil.rmtree(source)
+        shutil.rmtree(dest)
+
+
+def test_when_a_file_has_been_renamed_in_the_source():
+    try:
+        source = tempfile.mkdtemp()
+        dest = tempfile.mkdtemp()
+
+        content = "I am a file that was renamed"
+        source_path = Path(source) / "source-filename"
+        old_dest_path = Path(dest) / "dest-filename"
+        expected_dest_path = Path(dest) / "source-filename"
+        source_path.write_text(content)
+        old_dest_path.write_text(content)
+
+        sync(source, dest)
+
+        assert old_dest_path.exists() is False
+        assert expected_dest_path.read_text() == content
+
+    finally:
+        shutil.rmtree(source)
+        shutil.rmtree(dest)

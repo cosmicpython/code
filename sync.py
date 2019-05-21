808e5672390882dbf5e5fecc9f53ff1d2a81b800
Harry <hjwp2@cantab.net>
2019-05-21 12:13:42 +0100

    first cut with pathlib [sync_first_cut]

---
+++ sync.py
@@ -0,0 +1,39 @@
+import hashlib
+import os
+import shutil
+from pathlib import Path
+
+def sync(source, dest):
+    source_hashes = {}
+    for folder, _, files in os.walk(source):
+        for fn in files:
+            source_hashes[hash_file(Path(folder) / fn)] = fn
+
+    seen = set()
+    for folder, _, files in os.walk(dest):
+        for fn in files:
+            dest_path = Path(folder) / fn
+            dest_hash = hash_file(dest_path)
+            seen.add(dest_hash)
+
+            if dest_hash not in source_hashes:
+                dest_path.remove()
+            elif dest_hash in source_hashes and fn != source_hashes[dest_hash]:
+                shutil.move(dest_path, Path(folder) / source_hashes[dest_hash])
+
+    for src_hash, fn in source_hashes.items():
+        if src_hash not in seen:
+            shutil.copy(Path(source) / fn, Path(dest) / fn)
+
+
+BLOCKSIZE = 65536
+
+def hash_file(path):
+    hasher = hashlib.sha1()
+    with path.open("rb") as file:
+        buf = file.read(BLOCKSIZE)
+        while buf:
+            hasher.update(buf)
+            buf = file.read(BLOCKSIZE)
+    return hasher.hexdigest()
+

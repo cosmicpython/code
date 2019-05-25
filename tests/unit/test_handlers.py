d13475bfa2cb9e18f82d6a27c26f04815f13aa2f
Harry <hjwp2@cantab.net>
2019-05-25 10:11:42 +0100

    new test and put them into classes [test_change_batch_quantity_handler]

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -1,4 +1,5 @@
 # pylint: disable=no-self-use
+from datetime import date
 from unittest import mock
 import pytest
 
@@ -84,3 +85,38 @@ class TestAllocate:
             assert mock_send_mail.call_args == mock.call(
                 "stock@made.com", f"Out of stock for POPULAR-CURTAINS"
             )
+
+
+class TestChangeBatchQuantity:
+    def test_changes_available_quantity(self):
+        uow = FakeUnitOfWork()
+        messagebus.handle(
+            events.BatchCreated("batch1", "ADORABLE-SETTEE", 100, None), uow
+        )
+        [batch] = uow.products.get(sku="ADORABLE-SETTEE").batches
+        assert batch.available_quantity == 100
+
+        messagebus.handle(events.BatchQuantityChanged("batch1", 50), uow)
+
+        assert batch.available_quantity == 50
+
+    def test_reallocates_if_necessary(self):
+        uow = FakeUnitOfWork()
+        event_history = [
+            events.BatchCreated("batch1", "INDIFFERENT-TABLE", 50, None),
+            events.BatchCreated("batch2", "INDIFFERENT-TABLE", 50, date.today()),
+            events.AllocationRequired("order1", "INDIFFERENT-TABLE", 20),
+            events.AllocationRequired("order2", "INDIFFERENT-TABLE", 20),
+        ]
+        for e in event_history:
+            messagebus.handle(e, uow)
+        [batch1, batch2] = uow.products.get(sku="INDIFFERENT-TABLE").batches
+        assert batch1.available_quantity == 10
+        assert batch2.available_quantity == 50
+
+        messagebus.handle(events.BatchQuantityChanged("batch1", 25), uow)
+
+        # order1 or order2 will be deallocated, so we'll have 25 - 20
+        assert batch1.available_quantity == 5
+        # and 20 will be reallocated to the next batch
+        assert batch2.available_quantity == 30

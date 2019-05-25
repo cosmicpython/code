4990c4b13c37940639db9ab93e8eed7fa13d7258
Harry <hjwp2@cantab.net>
2019-05-25 18:01:26 +0100

    change_batch_quantity on product, needed change to batch, also emit allocated event. [change_batch_model_layer]

--- src/allocation/model.py
+++ src/allocation/model.py
@@ -25,6 +25,14 @@ class Product:
             self.events.append(events.OutOfStock(line.sku))
             return None
 
+    def change_batch_quantity(self, ref: str, qty: int):
+        batch = next(b for b in self.batches if b.reference == ref)
+        batch._purchased_quantity = qty
+        while batch.available_quantity < 0:
+            line = batch.deallocate_one()
+            self.events.append(
+                events.AllocationRequired(line.orderid, line.sku, line.qty)
+            )
 
 @dataclass(unsafe_hash=True)
 class OrderLine:
@@ -65,9 +73,8 @@ class Batch:
         if self.can_allocate(line):
             self._allocations.add(line)
 
-    def deallocate(self, line: OrderLine):
-        if line in self._allocations:
-            self._allocations.remove(line)
+    def deallocate_one(self) -> OrderLine:
+        return self._allocations.pop()
 
     @property
     def allocated_quantity(self) -> int:

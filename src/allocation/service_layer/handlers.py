43d796176ff06cb91ed27013d126c24672bd9d26
Harry <hjwp2@cantab.net>
2019-05-25 12:14:35 +0100

    start on handler for change quantity [change_quantity_handler]

--- src/allocation/service_layer/handlers.py
+++ src/allocation/service_layer/handlers.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+from allocation.adapters import email
 from allocation.domain import events, model
 from allocation.domain.model import OrderLine
 
@@ -38,6 +39,16 @@ def allocate(
         batchref = product.allocate(line)
         uow.commit()
         return batchref
+
+
+def change_batch_quantity(
+    event: events.BatchQuantityChanged,
+    uow: unit_of_work.AbstractUnitOfWork,
+):
+    with uow:
+        product = uow.products.get_by_batchref(batchref=event.ref)
+        product.change_batch_quantity(ref=event.ref, qty=event.qty)
+        uow.commit()
 
 
 # pylint: disable=unused-argument
--- src/allocation/service_layer/messagebus.py
+++ src/allocation/service_layer/messagebus.py
@@ -23,6 +23,7 @@ def handle(
 
 HANDLERS = {
     events.BatchCreated: [handlers.add_batch],
+    events.BatchQuantityChanged: [handlers.change_batch_quantity],
     events.AllocationRequired: [handlers.allocate],
     events.OutOfStock: [handlers.send_out_of_stock_notification],
 }  # type: Dict[Type[events.Event], List[Callable]]

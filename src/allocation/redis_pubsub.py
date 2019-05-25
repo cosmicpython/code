8b8e510a3bfd84bd84bcf8aa1c25eadec53c7728
Harry <hjwp2@cantab.net>
2019-05-25 10:04:47 +0100

    first cut of redis pubsub [redis_pubsub_first_cut]

---
+++ src/allocation/redis_pubsub.py
@@ -0,0 +1,34 @@
+import json
+from dataclasses import asdict
+import redis
+
+from allocation import config, orm, services, unit_of_work
+
+r = redis.Redis(**config.get_redis_host_and_port())
+
+
+def main():
+    orm.start_mappers()
+    pubsub = r.pubsub(ignore_subscribe_messages=True)
+    pubsub.subscribe('change_batch_quantity')
+
+    for m in pubsub.listen():
+        handle_change_batch_quantity(m)
+
+
+def handle_change_batch_quantity(m):
+    print('handling', m, flush=True)
+    data = json.loads(m['data'])
+    services.change_batch_quantity(
+        ref=data['batchref'], qty=data['qty'],
+        uow=unit_of_work.SqlAlchemyUnitOfWork(),
+    )
+
+
+def publish(channel, event):
+    print('publishing', channel, event, flush=True)
+    r.publish(channel, json.dumps(asdict(event)))
+
+if __name__ == '__main__':
+    main()
+

ec4e4a9827ba5e0e4840e0933d05e8d35b22f65d
Harry <hjwp2@cantab.net>
2019-05-26 21:29:23 +0100

    allocated and deallocated events [two_new_events]

--- src/allocation/events.py
+++ src/allocation/events.py
@@ -7,3 +7,18 @@ class Event:
 class OutOfStock(Event):
     sku: str
 
+
+@dataclass
+class Allocated(Event):
+    orderid: str
+    sku: str
+    qty: int
+    batchref: str
+
+
+@dataclass
+class Deallocated(Event):
+    orderid: str
+    sku: str
+    qty: int
+

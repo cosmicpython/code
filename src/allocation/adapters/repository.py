71a6fa34af07669df4814dead1e3731ef65297f7
Harry <hjwp2@cantab.net>
2019-05-28 10:40:46 +0100

    get_by_batchref in abstract+real repo, and a new integration test for it [get_by_batchref]

--- src/allocation/adapters/repository.py
+++ src/allocation/adapters/repository.py
@@ -1,5 +1,6 @@
 import abc
 from typing import Set
+from allocation.adapters import orm
 from allocation.domain import model
 
 
@@ -17,12 +18,22 @@ class AbstractRepository(abc.ABC):
             self.seen.add(product)
         return product
 
+    def get_by_batchref(self, batchref) -> model.Product:
+        product = self._get_by_batchref(batchref)
+        if product:
+            self.seen.add(product)
+        return product
+
     @abc.abstractmethod
     def _add(self, product: model.Product):
         raise NotImplementedError
 
     @abc.abstractmethod
     def _get(self, sku) -> model.Product:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _get_by_batchref(self, batchref) -> model.Product:
         raise NotImplementedError
 
 
@@ -36,3 +47,11 @@ class SqlAlchemyRepository(AbstractRepository):
 
     def _get(self, sku):
         return self.session.query(model.Product).filter_by(sku=sku).first()
+
+    def _get_by_batchref(self, batchref):
+        return (
+            self.session.query(model.Product)
+            .join(model.Batch)
+            .filter(orm.batches.c.reference == batchref)
+            .first()
+        )

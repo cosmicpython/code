7706833e0043aa6b6017367c6a5b644a72c9787c
Harry <hjwp2@cantab.net>
2019-06-09 09:46:47 +0100

    implement .seen on repository [repository_tracks_seen]

--- src/allocation/adapters/repository.py
+++ src/allocation/adapters/repository.py
@@ -5,12 +5,25 @@ from allocation.domain import model
 
 class AbstractRepository(abc.ABC):
 
-    @abc.abstractmethod
+    def __init__(self):
+        self.seen = set()  # type: Set[model.Product]
+
     def add(self, product: model.Product):
+        self._add(product)
+        self.seen.add(product)
+
+    def get(self, sku) -> model.Product:
+        product = self._get(sku)
+        if product:
+            self.seen.add(product)
+        return product
+
+    @abc.abstractmethod
+    def _add(self, product: model.Product):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, sku) -> model.Product:
+    def _get(self, sku) -> model.Product:
         raise NotImplementedError
 
 
@@ -18,15 +31,11 @@ class AbstractRepository(abc.ABC):
 class SqlAlchemyRepository(AbstractRepository):
 
     def __init__(self, session):
+        super().__init__()
         self.session = session
-        self.seen = set()  # type: Set[model.Product]
 
-    def add(self, product):
-        self.seen.add(product)
+    def _add(self, product):
         self.session.add(product)
 
-    def get(self, sku):
-        product = self.session.query(model.Product).filter_by(sku=sku).first()
-        if product:
-            self.seen.add(product)
-        return product
+    def _get(self, sku):
+        return self.session.query(model.Product).filter_by(sku=sku).first()

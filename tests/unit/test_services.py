7cc9094861a3146f40309296e9f6165bfa82946b
Harry <hjwp2@cantab.net>
2019-06-09 09:46:47 +0100

    implement .seen on repository [repository_tracks_seen]

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -7,12 +7,13 @@ from allocation.service_layer import services, unit_of_work
 class FakeRepository(repository.AbstractRepository):
 
     def __init__(self, products):
+        super().__init__()
         self._products = set(products)
 
-    def add(self, product):
+    def _add(self, product):
         self._products.add(product)
 
-    def get(self, sku):
+    def _get(self, sku):
         return next((p for p in self._products if p.sku == sku), None)
 
 

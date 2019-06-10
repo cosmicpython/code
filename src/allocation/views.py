ef5fde6c50a132a3d22c3a77702c710d2044dd38
Harry <hjwp2@cantab.net>
2019-06-10 21:50:25 +0100

    first cut of a view with raw sql [views_dot_py]

---
+++ src/allocation/views.py
@@ -0,0 +1,14 @@
+from allocation import unit_of_work
+
+def allocations(orderid: str, uow: unit_of_work.SqlAlchemyUnitOfWork):
+    with uow:
+        results = list(uow.session.execute(
+            'SELECT ol.sku, b.reference'
+            ' FROM allocations AS a'
+            ' JOIN batches AS b ON a.batch_id = b.id'
+            ' JOIN order_lines AS ol ON a.orderline_id = ol.id'
+            ' WHERE ol.orderid = :orderid',
+            dict(orderid=orderid)
+        ))
+        print('results', results, flush=True)
+    return [{'sku': sku, 'batchref': batchref} for sku, batchref in results]

1fca005271727548e533812f1b97c3af1c4699e7
Harry <hjwp2@cantab.net>
2019-06-10 22:33:43 +0100

    new table in orm

--- src/allocation/adapters/orm.py
+++ src/allocation/adapters/orm.py
@@ -49,6 +49,14 @@ allocations = Table(
     Column("batch_id", ForeignKey("batches.id")),
 )
 
+allocations_view = Table(
+    "allocations_view",
+    metadata,
+    Column("orderid", String(255)),
+    Column("sku", String(255)),
+    Column("batchref", String(255)),
+)
+
 
 def start_mappers():
     lines_mapper = mapper(model.OrderLine, order_lines)

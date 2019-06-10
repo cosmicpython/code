7ac1cf4965489a8df92128c9610bc526f45c3788
Harry <hjwp2@cantab.net>
2019-06-10 21:50:01 +0100

    modify flask to add new view endpoint and return 202s

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -5,6 +5,7 @@ from allocation.domain import commands
 from allocation.adapters import orm
 from allocation.service_layer import messagebus, unit_of_work
 from allocation.service_layer.handlers import InvalidSku
+from allocation import views
 
 app = Flask(__name__)
 orm.start_mappers()
@@ -35,4 +36,13 @@ def allocate_endpoint():
     except InvalidSku as e:
         return jsonify({'message': str(e)}), 400
 
-    return jsonify({'batchref': batchref}), 201
+    return 'OK', 202
+
+
+@app.route("/allocations/<orderid>", methods=['GET'])
+def allocations_view_endpoint(orderid):
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
+    result = views.allocations(orderid, uow)
+    if not result:
+        return 'not found', 404
+    return jsonify(result), 200

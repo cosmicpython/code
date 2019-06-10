0dd61333a433a9084b29859af15be9935359b644
Harry <hjwp2@cantab.net>
2019-06-10 22:33:58 +0100

    handler for view model update

--- src/allocation/views.py
+++ src/allocation/views.py
@@ -3,10 +3,10 @@ from allocation.service_layer import unit_of_work
 
 def allocations(orderid: str, uow: unit_of_work.SqlAlchemyUnitOfWork):
     with uow:
-        results = list(
-            uow.session.execute(
-                "SELECT sku, batchref FROM allocations_view WHERE orderid = :orderid",
-                dict(orderid=orderid),
-            )
+        results = uow.session.execute(
+            """
+            SELECT sku, batchref FROM allocations_view WHERE orderid = :orderid
+            """,
+            dict(orderid=orderid),
         )
     return [dict(r) for r in results]

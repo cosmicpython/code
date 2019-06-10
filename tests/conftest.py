cff6a669d47f7fbb153ca55ed42f8ffea4a40f98
Harry <hjwp2@cantab.net>
2019-06-10 21:22:46 +0100

    modify api tests to try and do a get after a post [get_after_post]

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ from tenacity import retry, stop_after_delay
 from allocation.adapters.orm import metadata, start_mappers
 from allocation import config
 
+pytest.register_assert_rewrite('tests.e2e.api_client')
 
 @pytest.fixture
 def in_memory_db():

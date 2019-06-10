8de93ec37451ae26d0bd973aee30aac81942ead9
Harry <hjwp2@cantab.net>
2019-06-10 22:33:33 +0100

    Simpler view based on a new read model table

--- src/allocation/views.py
+++ src/allocation/views.py
@@ -1,11 +1,9 @@
-from allocation.domain import model
 from allocation.service_layer import unit_of_work
 
 def allocations(orderid: str, uow: unit_of_work.SqlAlchemyUnitOfWork):
     with uow:
-        batches = uow.session.query(model.Batch).join(
-            model.OrderLine, model.Batch._allocations
-        ).filter(
-            model.OrderLine.orderid == orderid
-        )
-        return [{'sku': b.sku, 'batchref': b.reference} for b in batches]
+        results = list(uow.session.execute(
+            'SELECT sku, batchref FROM allocations_view WHERE orderid = :orderid',
+            dict(orderid=orderid)
+        ))
+    return [dict(r) for r in results]

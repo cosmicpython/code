f6833ffe58b3fd71b1c243aa687722efa5c8fd49
Harry <hjwp2@cantab.net>
2019-06-10 21:22:46 +0100

    modify api tests to try and do a get after a post [get_after_post]

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -5,7 +5,8 @@ from . import api_client
 
 @pytest.mark.usefixtures("postgres_db")
 @pytest.mark.usefixtures("restart_api")
-def test_happy_path_returns_201_and_allocated_batch():
+def test_happy_path_returns_202_and_batch_is_allocated():
+    orderid = random_orderid()
     sku, othersku = random_sku(), random_sku("other")
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
@@ -14,23 +15,25 @@ def test_happy_path_returns_201_and_allocated_batch():
     api_client.post_to_add_batch(earlybatch, sku, 100, "2011-01-01")
     api_client.post_to_add_batch(otherbatch, othersku, 100, None)
 
-    response = api_client.post_to_allocate(random_orderid(), sku, qty=3)
+    r = api_client.post_to_allocate(orderid, sku, qty=3)
+    assert r.status_code == 202
 
-    assert response.status_code == 201
-    assert response.json()["batchref"] == earlybatch
+    r = api_client.get_allocation(orderid)
+    assert r.ok
+    assert r.json() == [
+        {"sku": sku, "batchref": earlybatch},
+    ]
 
 
 @pytest.mark.usefixtures("postgres_db")
 @pytest.mark.usefixtures("restart_api")
 def test_unhappy_path_returns_400_and_error_message():
     unknown_sku, orderid = random_sku(), random_orderid()
-
-    response = api_client.post_to_allocate(
-        orderid,
-        unknown_sku,
-        qty=20,
-        expect_success=False,
+    r = api_client.post_to_allocate(
+        orderid, unknown_sku, qty=20, expect_success=False
     )
+    assert r.status_code == 400
+    assert r.json()["message"] == f"Invalid sku {unknown_sku}"
 
-    assert response.status_code == 400
-    assert response.json()["message"] == f"Invalid sku {unknown_sku}"
+    r = api_client.get_allocation(orderid)
+    assert r.status_code == 404

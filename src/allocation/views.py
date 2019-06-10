351ba86fbe047fcdcd27aa41efd5571c178175fc
Harry <hjwp2@cantab.net>
2019-06-10 22:33:33 +0100

    simpler view based on a new read model table

--- src/allocation/views.py
+++ src/allocation/views.py
@@ -3,12 +3,8 @@ from allocation import unit_of_work
 def allocations(orderid: str, uow: unit_of_work.SqlAlchemyUnitOfWork):
     with uow:
         results = list(uow.session.execute(
-            'SELECT ol.sku, b.reference'
-            ' FROM allocations AS a'
-            ' JOIN batches AS b ON a.batch_id = b.id'
-            ' JOIN order_lines AS ol ON a.orderline_id = ol.id'
-            ' WHERE ol.orderid = :orderid',
+            'SELECT sku, batchref FROM allocations_view WHERE orderid = :orderid',
             dict(orderid=orderid)
         ))
         print('results', results, flush=True)
-    return [{'sku': sku, 'batchref': batchref} for sku, batchref in results]
+    return [dict(r) for r in results]

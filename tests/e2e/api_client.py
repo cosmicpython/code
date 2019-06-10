afe12a34ea6576f9d94590e87b368da1aee34ce2
Harry <hjwp2@cantab.net>
2019-06-10 21:22:46 +0100

    modify api tests to try and do a get after a post [get_after_post]

--- tests/e2e/api_client.py
+++ tests/e2e/api_client.py
@@ -17,5 +17,9 @@ def post_to_allocate(orderid, sku, qty, expect_success=True):
         'orderid': orderid, 'sku': sku, 'qty': qty,
     })
     if expect_success:
-        assert r.status_code == 201
+        assert r.status_code == 202
     return r
+
+def get_allocation(orderid):
+    url = config.get_api_url()
+    return requests.get(f'{url}/allocations/{orderid}')

18a4e94473d470a592c9b87fd43c8f6ac5902cce
Harry <hjwp2@cantab.net>
2019-06-10 22:43:35 +0100

    make sure deallocation fixes view model too [deallocation_to_readmodel]

--- tests/integration/test_views.py
+++ tests/integration/test_views.py
@@ -19,3 +19,15 @@ def test_allocations_view(sqlite_session_factory):
         {'sku': 'sku1', 'batchref': 'sku1batch'},
         {'sku': 'sku2', 'batchref': 'sku2batch'},
     ]
+
+
+def test_deallocation(sqlite_session_factory):
+    uow = unit_of_work.SqlAlchemyUnitOfWork(sqlite_session_factory)
+    messagebus.handle(commands.CreateBatch('b1', 'sku1', 50, None), uow)
+    messagebus.handle(commands.CreateBatch('b2', 'sku1', 50, date.today()), uow)
+    messagebus.handle(commands.Allocate('o1', 'sku1', 40), uow)
+    messagebus.handle(commands.ChangeBatchQuantity('b1', 10), uow)
+
+    assert views.allocations('o1', uow) == [
+        {'sku': 'sku1', 'batchref': 'b2'},
+    ]

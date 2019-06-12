3a55cde57ba2ddee9e0c4e658daac9da3063fb0e
Harry <hjwp2@cantab.net>
2019-06-12 21:57:35 +0100

    session_factory -> sqlite_session_factory (needs backport)

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -34,12 +34,12 @@ def get_allocated_batch_ref(session, orderid, sku):
     return batchref
 
 
-def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
-    session = session_factory()
+def test_uow_can_retrieve_a_batch_and_allocate_to_it(sqlite_session_factory):
+    session = sqlite_session_factory()
     insert_batch(session, "batch1", "HIPSTER-WORKBENCH", 100, None)
     session.commit()
 
-    uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
+    uow = unit_of_work.SqlAlchemyUnitOfWork(sqlite_session_factory)
     with uow:
         product = uow.products.get(sku="HIPSTER-WORKBENCH")
         line = model.OrderLine("o1", "HIPSTER-WORKBENCH", 10)
@@ -50,27 +50,27 @@ def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
     assert batchref == "batch1"
 
 
-def test_rolls_back_uncommitted_work_by_default(session_factory):
-    uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
+def test_rolls_back_uncommitted_work_by_default(sqlite_session_factory):
+    uow = unit_of_work.SqlAlchemyUnitOfWork(sqlite_session_factory)
     with uow:
         insert_batch(uow.session, "batch1", "MEDIUM-PLINTH", 100, None)
 
-    new_session = session_factory()
+    new_session = sqlite_session_factory()
     rows = list(new_session.execute('SELECT * FROM "batches"'))
     assert rows == []
 
 
-def test_rolls_back_on_error(session_factory):
+def test_rolls_back_on_error(sqlite_session_factory):
     class MyException(Exception):
         pass
 
-    uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
+    uow = unit_of_work.SqlAlchemyUnitOfWork(sqlite_session_factory)
     with pytest.raises(MyException):
         with uow:
             insert_batch(uow.session, "batch1", "LARGE-FORK", 100, None)
             raise MyException()
 
-    new_session = session_factory()
+    new_session = sqlite_session_factory()
     rows = list(new_session.execute('SELECT * FROM "batches"'))
     assert rows == []
 

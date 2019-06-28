73f3576c0afb4f0ce70b993e7f08652ccf189953
Harry <hjwp2@cantab.net>
2019-06-28 15:17:32 +0100

    new service test for deallocation

--- test_services.py
+++ test_services.py
@@ -51,3 +51,24 @@ def test_commits():
 
     services.allocate(line, repo, session)
     assert session.committed is True
+
+
+def test_deallocate_decrements_available_quantity():
+    repo, session = FakeRepository([]), FakeSession()
+    # TODO: you'll need to implement the services.add_batch method
+    services.add_batch("b1", "BLUE-PLINTH", 100, None, repo, session)
+    line = model.OrderLine("o1", "BLUE-PLINTH", 10)
+    services.allocate(line, repo, session)
+    batch = repo.get(reference="b1")
+    assert batch.available_quantity == 90
+    # services.deallocate(...
+    ...
+    assert batch.available_quantity == 100
+
+
+def test_deallocate_decrements_correct_quantity():
+    ...  #  TODO - check that we decrement the right sku
+
+
+def test_trying_to_deallocate_unallocated_batch():
+    ...  #  TODO: should this error or pass silently? up to you.

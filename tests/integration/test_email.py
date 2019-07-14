ecba432a51b479bded9d8daed82ec0563e7c2086
Harry <hjwp2@cantab.net>
2019-07-14 03:59:18 +0100

    fixup cfg in test_email for shortening

--- tests/integration/test_email.py
+++ tests/integration/test_email.py
@@ -4,15 +4,15 @@ import pytest
 import requests
 from allocation import config, commands, messagebus, notifications, unit_of_work
 
-email_config = config.get_email_host_and_port()
+cfg = config.get_email_host_and_port()
 
 @pytest.fixture
 def bus(sqlite_session_factory):
     return messagebus.MessageBus(
         uow=unit_of_work.SqlAlchemyUnitOfWork(sqlite_session_factory),
         notifications=notifications.EmailNotifications(
-            smtp_host=email_config['host'],
-            port=email_config['port'],
+            smtp_host=cfg['host'],
+            port=cfg['port'],
         ),
         publish=lambda *_, **__: None
     )
@@ -29,7 +29,7 @@ def test_out_of_stock_email(bus):
         commands.Allocate('order1', sku, 10),
     ])
     messages = requests.get(
-        f'http://{email_config["host"]}:{email_config["http_port"]}/api/v2/messages'
+        f'http://{cfg["host"]}:{cfg["http_port"]}/api/v2/messages'
     ).json()
     message = next(
         m for m in messages['items']

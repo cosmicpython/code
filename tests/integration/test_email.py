fac26c706057719f1f036cdc21d6ed505450cd5c
Harry <hjwp2@cantab.net>
2019-07-14 03:59:18 +0100

    fixup cfg in test_email for shortening [chapter_12_dependency_injection_ends]

--- tests/integration/test_email.py
+++ tests/integration/test_email.py
@@ -4,7 +4,7 @@ import pytest
 import requests
 from allocation import config, commands, messagebus, notifications, unit_of_work
 
-email_config = config.get_email_host_and_port()
+cfg = config.get_email_host_and_port()
 
 @pytest.fixture
 def bus(sqlite_session_factory):
@@ -12,8 +12,8 @@ def bus(sqlite_session_factory):
     bus = messagebus.MessageBus(
         uow=uow,
         notifications=notifications.EmailNotifications(
-            smtp_host=email_config['host'],
-            port=email_config['port'],
+            smtp_host=cfg['host'],
+            port=cfg['port'],
         ),
         publish=lambda *_, **__: None
     )
@@ -30,7 +30,7 @@ def test_out_of_stock_email(bus):
     bus.handle(commands.CreateBatch('batch1', sku, 9, None))
     bus.handle(commands.Allocate('order1', sku, 10))
     messages = requests.get(
-        f'http://{email_config["host"]}:{email_config["http_port"]}/api/v2/messages'
+        f'http://{cfg["host"]}:{cfg["http_port"]}/api/v2/messages'
     ).json()
     message = next(
         m for m in messages['items']

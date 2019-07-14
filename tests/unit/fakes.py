a2b76404a55e1c374dd883dfbb0ef6199e6ec266
Harry <hjwp2@cantab.net>
2019-07-14 02:56:00 +0100

    tests for notifcations

---
+++ tests/unit/fakes.py
@@ -0,0 +1,58 @@
+from typing import Dict, List
+from collections import defaultdict
+from unittest import mock
+from allocation.adapters import notifications, repository
+from allocation.service_layer import messagebus, unit_of_work
+
+
+class FakeRepository(repository.AbstractRepository):
+
+    def __init__(self, products):
+        super().__init__()
+        self._products = set(products)
+
+    def _add(self, product):
+        self._products.add(product)
+
+    def _get(self, sku):
+        return next((p for p in self._products if p.sku == sku), None)
+
+    def _get_by_batchref(self, batchref):
+        return next((
+            p for p in self._products for b in p.batches
+            if b.reference == batchref
+        ), None)
+
+
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
+
+    def __init__(self):
+        self.products = FakeRepository([])
+        self.committed = False
+
+    def _commit(self):
+        self.committed = True
+
+    def rollback(self):
+        pass
+
+
+
+class FakeBus(messagebus.MessageBus):
+    def __init__(self):
+        uow = FakeUnitOfWork()
+        super().__init__(
+            uow=uow,
+            notifications=FakeNotifications(),
+            publish=mock.Mock(),
+        )
+        uow.bus = self
+
+
+class FakeNotifications(notifications.AbstractNotifications):
+
+    def __init__(self):
+        self.sent = defaultdict(list)  # type: Dict[str, List[str]]
+
+    def send(self, destination, message):
+        self.sent[destination].append(message)

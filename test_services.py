1fd431e2bce8b92e436e224fe2fe0e000dafd5ea
Harry <hjwp2@cantab.net>
2019-07-18 13:21:43 +0100

    Test for commits [second_services_test]

--- test_services.py
+++ test_services.py
@@ -45,8 +45,8 @@ def test_error_for_invalid_sku():
 
 
 def test_commits():
-    line = model.OrderLine('o1', 'sku1', 10)
-    batch = model.Batch('b1', 'sku1', 100, eta=None)
+    line = model.OrderLine('o1', 'OMINOUS-MIRROR', 10)
+    batch = model.Batch('b1', 'OMINOUS-MIRROR', 100, eta=None)
     repo = FakeRepository([batch])
     session = FakeSession()
 

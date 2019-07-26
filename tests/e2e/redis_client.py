7d7bc950424a8811ada61e58ff6aaaf0ffca330d
Harry <hjwp2@cantab.net>
2019-07-26 15:02:41 +0100

    redis client for tests [redis_client_for_tests]

---
+++ tests/e2e/redis_client.py
@@ -0,0 +1,18 @@
+import json
+import redis
+
+from allocation import config
+
+r = redis.Redis(**config.get_redis_host_and_port())
+
+
+def subscribe_to(channel):
+    pubsub = r.pubsub()
+    pubsub.subscribe(channel)
+    confirmation = pubsub.get_message(timeout=3)
+    assert confirmation["type"] == "subscribe"
+    return pubsub
+
+
+def publish_message(channel, message):
+    r.publish(channel, json.dumps(message))

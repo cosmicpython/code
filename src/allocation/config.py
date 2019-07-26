4316cd14958f8eae75dcdd8e17ecc1a5f58049be
Harry <hjwp2@cantab.net>
2019-07-26 14:58:39 +0100

    Add redis to docker-compose and config, conftest

--- src/allocation/config.py
+++ src/allocation/config.py
@@ -13,3 +13,9 @@ def get_api_url():
     host = os.environ.get("API_HOST", "localhost")
     port = 5005 if host == "localhost" else 80
     return f"http://{host}:{port}"
+
+
+def get_redis_host_and_port():
+    host = os.environ.get("REDIS_HOST", "localhost")
+    port = 63791 if host == "localhost" else 6379
+    return dict(host=host, port=port)

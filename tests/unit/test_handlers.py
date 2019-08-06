2fc51b5660a322b9ca46cc558da0d8a6f0ca8cef
Harry <hjwp2@cantab.net>
2019-08-06 13:51:51 +0100

    change handler/services tests to use events [handler_tests]

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -1,6 +1,6 @@
 from unittest import mock
 import pytest
-from allocation import services, exceptions, repository, unit_of_work
+from allocation import events, exceptions, messagebus, repository, unit_of_work
 
 
 class FakeRepository(repository.AbstractRepository):
@@ -30,49 +30,57 @@ class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
 
 
 
-def test_add_batch_for_new_product():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
-    assert uow.products.get("CRUNCHY-ARMCHAIR") is not None
-    assert uow.committed
+class TestAddBatch:
+
+    @staticmethod
+    def test_for_new_product():
+        uow = FakeUnitOfWork()
+        messagebus.handle(events.BatchCreated("b1", "CRUNCHY-ARMCHAIR", 100, None), uow)
+        assert uow.products.get("CRUNCHY-ARMCHAIR") is not None
+        assert uow.committed
+
+    @staticmethod
+    def test_for_existing_product():
+        uow = FakeUnitOfWork()
+        messagebus.handle(events.BatchCreated("b1", "GARISH-RUG", 100, None), uow)
+        messagebus.handle(events.BatchCreated("b2", "GARISH-RUG", 99, None), uow)
+        assert "b2" in [b.reference for b in uow.products.get("GARISH-RUG").batches]
 
 
-def test_add_batch_for_existing_product():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "GARISH-RUG", 100, None, uow)
-    services.add_batch("b2", "GARISH-RUG", 99, None, uow)
-    assert "b2" in [b.reference for b in uow.products.get("GARISH-RUG").batches]
+
+class TestAllocate:
+
+    @staticmethod
+    def test_returns_allocation():
+        uow = FakeUnitOfWork()
+        messagebus.handle(events.BatchCreated("b1", "COMPLICATED-LAMP", 100, None), uow)
+        result = messagebus.handle(events.AllocationRequired("o1", "COMPLICATED-LAMP", 10), uow)
+        assert result == "b1"
+
+    @staticmethod
+    def test_errors_for_invalid_sku():
+        uow = FakeUnitOfWork()
+        messagebus.handle(events.BatchCreated("b1", "AREALSKU", 100, None), uow)
+
+        with pytest.raises(exceptions.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+            messagebus.handle(events.AllocationRequired("o1", "NONEXISTENTSKU", 10) , uow)
 
 
-def test_allocate_returns_allocation():
-    uow = FakeUnitOfWork()
-    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
-    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
-    assert result == "batch1"
+    @staticmethod
+    def test_commits():
+        uow = FakeUnitOfWork()
+        messagebus.handle(events.BatchCreated("b1", "OMINOUS-MIRROR", 100, None), uow)
+        messagebus.handle(events.AllocationRequired("o1", "OMINOUS-MIRROR", 10), uow)
+        assert uow.committed
 
+    @staticmethod
+    def test_sends_email_on_out_of_stock_error():
+        uow = FakeUnitOfWork()
+        messagebus.handle(events.BatchCreated("b1", "POPULAR-CURTAINS", 9, None), uow)
 
-def test_allocate_errors_for_invalid_sku():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "AREALSKU", 100, None, uow)
-
-    with pytest.raises(exceptions.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate("o1", "NONEXISTENTSKU", 10, uow)
-
-
-def test_allocate_commits():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
-    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
-    assert uow.committed
-
-
-def test_sends_email_on_out_of_stock_error():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "POPULAR-CURTAINS", 9, None, uow)
-
-    with mock.patch("allocation.email.send_mail") as mock_send_mail:
-        services.allocate("o1", "POPULAR-CURTAINS", 10, uow)
-        assert mock_send_mail.call_args == mock.call(
-            "stock@made.com",
-            f"Out of stock for POPULAR-CURTAINS",
-        )
+        with mock.patch("allocation.email.send") as mock_send_mail:
+            messagebus.handle(events.AllocationRequired("o1", "POPULAR-CURTAINS", 10), uow)
+            assert mock_send_mail.call_args == mock.call(
+                "stock@made.com",
+                f"Out of stock for POPULAR-CURTAINS",
+            )

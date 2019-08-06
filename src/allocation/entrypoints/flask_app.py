33c4089dd4fc7055368306cc2518e9f430b5ce37
Harry <hjwp2@cantab.net>
2019-08-06 12:33:23 +0100

    modify flask to use messagebus [flask_uses_messagebus]

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from flask import Flask, jsonify, request
 
+from allocation.domain import events
 from allocation.adapters import orm
-from allocation.service_layer import services, unit_of_work
-from allocation.service_layer.services import InvalidSku
+from allocation.service_layer import messagebus, unit_of_work
+from allocation.service_layer.handlers import InvalidSku
 
 app = Flask(__name__)
 orm.start_mappers()
@@ -14,22 +15,21 @@ def add_batch():
     eta = request.json['eta']
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
-    services.add_batch(
+    event = events.BatchCreated(
         request.json['ref'], request.json['sku'], request.json['qty'], eta,
-        unit_of_work.SqlAlchemyUnitOfWork(),
     )
+    messagebus.handle(event, unit_of_work.SqlAlchemyUnitOfWork())
     return 'OK', 201
 
 
 @app.route("/allocate", methods=['POST'])
 def allocate_endpoint():
     try:
-        batchref = services.allocate(
-            request.json['orderid'],
-            request.json['sku'],
-            request.json['qty'],
-            unit_of_work.SqlAlchemyUnitOfWork(),
+        event = events.AllocationRequired(
+            request.json['orderid'], request.json['sku'], request.json['qty'],
         )
+        results = messagebus.handle(event, unit_of_work.SqlAlchemyUnitOfWork())
+        batchref = results.pop(0)
     except InvalidSku as e:
         return jsonify({'message': str(e)}), 400
 

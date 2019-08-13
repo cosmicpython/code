8fdf1197f5904d674b73a701c0e88ebff3dd5357
Harry <hjwp2@cantab.net>
2019-08-13 17:32:38 +0100

    remove old events

--- src/allocation/domain/events.py
+++ src/allocation/domain/events.py
@@ -1,32 +1,9 @@
 # pylint: disable=too-few-public-methods
 from dataclasses import dataclass
-from datetime import date
-from typing import Optional
 
 
 class Event:
     pass
-
-
-@dataclass
-class BatchCreated(Event):
-    ref: str
-    sku: str
-    qty: int
-    eta: Optional[date] = None
-
-
-@dataclass
-class BatchQuantityChanged(Event):
-    ref: str
-    qty: int
-
-
-@dataclass
-class AllocationRequired(Event):
-    orderid: str
-    sku: str
-    qty: int
 
 
 @dataclass

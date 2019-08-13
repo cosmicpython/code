c3da55bc7fc9031c0d7e4cbb9b9098b017cddb43
Harry <hjwp2@cantab.net>
2019-08-13 17:39:28 +0100

    flask now uses commands [chapter_10_commands_ends]

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Flask, jsonify, request
 
-from allocation.domain import events
+from allocation.domain import commands
 from allocation.adapters import orm
 from allocation.service_layer import messagebus, unit_of_work
 from allocation.service_layer.handlers import InvalidSku
@@ -15,20 +15,22 @@ def add_batch():
     eta = request.json['eta']
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
-    event = events.BatchCreated(
+    cmd = commands.CreateBatch(
         request.json['ref'], request.json['sku'], request.json['qty'], eta,
     )
-    messagebus.handle(event, unit_of_work.SqlAlchemyUnitOfWork())
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
+    messagebus.handle(cmd, uow)
     return 'OK', 201
 
 
 @app.route("/allocate", methods=['POST'])
 def allocate_endpoint():
     try:
-        event = events.AllocationRequired(
+        cmd = commands.Allocate(
             request.json['orderid'], request.json['sku'], request.json['qty'],
         )
-        results = messagebus.handle(event, unit_of_work.SqlAlchemyUnitOfWork())
+        uow = unit_of_work.SqlAlchemyUnitOfWork()
+        results = messagebus.handle(cmd, uow)
         batchref = results.pop(0)
     except InvalidSku as e:
         return jsonify({'message': str(e)}), 400

9c084369a9c42474f6308cdeaf2c3eb7c4cda5c6
Harry <hjwp2@cantab.net>
2019-08-13 17:13:56 +0100

    messagebus now does events and commands differently [new_messagebus]

--- src/allocation/service_layer/messagebus.py
+++ src/allocation/service_layer/messagebus.py
@@ -1,24 +1,53 @@
+# pylint: disable=bare-except
 from __future__ import annotations
-from typing import List, Dict, Callable, Type, TYPE_CHECKING
-from allocation.domain import events
+import logging
+from typing import List, Dict, Callable, Type, Union, TYPE_CHECKING
+from allocation.domain import commands, events
 from . import handlers
+
 if TYPE_CHECKING:
     from allocation.service_layer import unit_of_work
 
+logger = logging.getLogger(__name__)
 
-def handle(event: events.Event, uow: unit_of_work.AbstractUnitOfWork):
-    results = []
-    for handler in HANDLERS[type(event)]:
-        r = handler(event, uow=uow)
-        results.append(r)
-    return results
+Message = Union[commands.Command, events.Event]
 
 
+def handle(message: Message, uow: unit_of_work.AbstractUnitOfWork):
+    if isinstance(message, events.Event):
+        handle_event(message, uow)
+    elif isinstance(message, commands.Command):
+        return handle_command(message, uow)
+    else:
+        raise Exception(f'{message} was not an Event or Command')
 
-HANDLERS = {
-    events.BatchCreated: [handlers.add_batch],
-    events.BatchQuantityChanged: [handlers.change_batch_quantity],
-    events.AllocationRequired: [handlers.allocate],
+
+def handle_event(event: events.Event, uow: unit_of_work.AbstractUnitOfWork):
+    for handler in EVENT_HANDLERS[type(event)]:
+        try:
+            logger.debug('handling event %s with handler %s', event, handler)
+            handler(event, uow=uow)
+        except:
+            logger.exception('Exception handling event %s', event)
+            continue
+
+
+def handle_command(command, uow: unit_of_work.AbstractUnitOfWork):
+    logger.debug('handling command %s', command)
+    try:
+        handler = COMMAND_HANDLERS[type(command)]
+        return handler(command, uow=uow)
+    except Exception:
+        logger.exception('Exception handling command %s', command)
+        raise
+
+
+EVENT_HANDLERS = {
     events.OutOfStock: [handlers.send_out_of_stock_notification],
-
 }  # type: Dict[Type[events.Event], List[Callable]]
+
+COMMAND_HANDLERS = {
+    commands.Allocate: handlers.allocate,
+    commands.CreateBatch: handlers.add_batch,
+    commands.ChangeBatchQuantity: handlers.change_batch_quantity,
+}  # type: Dict[Type[commands.Command], Callable]

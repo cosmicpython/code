7d6be2cdc3a1af9d0fe83500e252b6f81c877186
Harry <hjwp2@cantab.net>
2019-08-13 06:20:52 +0100

    fix redis e2e test

--- tests/e2e/test_external_events.py
+++ tests/e2e/test_external_events.py
@@ -14,8 +14,10 @@ def test_change_batch_quantity_leading_to_reallocation():
     earlier_batch, later_batch = random_batchref("old"), random_batchref("newer")
     api_client.post_to_add_batch(earlier_batch, sku, qty=10, eta="2011-01-01")
     api_client.post_to_add_batch(later_batch, sku, qty=10, eta="2011-01-02")
-    response = api_client.post_to_allocate(orderid, sku, 10)
-    assert response.json()["batchref"] == earlier_batch
+    r = api_client.post_to_allocate(orderid, sku, 10)
+    assert r.ok
+    response = api_client.get_allocation(orderid)
+    assert response.json()[0]["batchref"] == earlier_batch
 
     subscription = redis_client.subscribe_to("line_allocated")
 

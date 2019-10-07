6a4cb0e16e9c4cccf9cbca3de2f0c68b727d9de7
Harry <hjwp2@cantab.net>
2019-10-07 14:23:29 +0100

    redis initialises bus [redis_initialises_bus]

--- src/allocation/entrypoints/redis_eventconsumer.py
+++ src/allocation/entrypoints/redis_eventconsumer.py
@@ -4,29 +4,40 @@ import redis
 
 from allocation import config
 from allocation.domain import commands
-from allocation.adapters import orm
+from allocation.adapters import email, orm, redis_eventpublisher
 from allocation.service_layer import messagebus, unit_of_work
 
 logger = logging.getLogger(__name__)
 
 r = redis.Redis(**config.get_redis_host_and_port())
 
+def get_bus():
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
+    bus = messagebus.MessageBus(
+        uow=uow,
+        send_mail=email.send,
+        publish=redis_eventpublisher.publish
+    )
+    uow.bus = bus
+    return bus
+
 
 def main():
-    orm.start_mappers()
     pubsub = r.pubsub(ignore_subscribe_messages=True)
     pubsub.subscribe('change_batch_quantity')
+    bus = get_bus()
 
     for m in pubsub.listen():
-        handle_change_batch_quantity(m)
+        handle_change_batch_quantity(m, bus)
 
 
-def handle_change_batch_quantity(m):
+def handle_change_batch_quantity(m, bus: messagebus.MessageBus):
     logging.debug('handling %s', m)
     data = json.loads(m['data'])
     cmd = commands.ChangeBatchQuantity(ref=data['batchref'], qty=data['qty'])
-    messagebus.handle(cmd, uow=unit_of_work.SqlAlchemyUnitOfWork())
+    bus.handle(cmd)
 
 
 if __name__ == '__main__':
+    orm.start_mappers()
     main()

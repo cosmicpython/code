bb51578bdd3860de31702011b2a858b16f60520b
Harry <hjwp2@cantab.net>
2019-10-08 12:56:58 +0100

    logging.info needs backport

--- src/allocation/adapters/redis_eventpublisher.py
+++ src/allocation/adapters/redis_eventpublisher.py
@@ -12,5 +12,5 @@ r = redis.Redis(**config.get_redis_host_and_port())
 
 
 def publish(channel, event: events.Event):
-    logging.debug("publishing: channel=%s, event=%s", channel, event)
+    logging.info("publishing: channel=%s, event=%s", channel, event)
     r.publish(channel, json.dumps(asdict(event)))

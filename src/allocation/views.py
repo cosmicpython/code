09035823abf6b4c689a7d9392b0f01308e5f2214
Harry <hjwp2@cantab.net>
2019-11-06 13:26:44 +0000

    use repository and go via Product [view_using_repo]

--- src/allocation/views.py
+++ src/allocation/views.py
@@ -2,12 +2,10 @@ from allocation.service_layer import unit_of_work
 
 def allocations(orderid: str, uow: unit_of_work.SqlAlchemyUnitOfWork):
     with uow:
-        results = list(uow.session.execute(
-            'SELECT ol.sku, b.reference'
-            ' FROM allocations AS a'
-            ' JOIN batches AS b ON a.batch_id = b.id'
-            ' JOIN order_lines AS ol ON a.orderline_id = ol.id'
-            ' WHERE ol.orderid = :orderid',
-            dict(orderid=orderid)
-        ))
-    return [{'sku': sku, 'batchref': batchref} for sku, batchref in results]
+        products = uow.products.for_order(orderid=orderid)
+        batches = [b for p in products for b in p.batches]
+        return [
+            {'sku': b.sku, 'batchref': b.reference}
+            for b in batches
+            if orderid in b.orderids
+        ]

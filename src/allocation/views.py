84a2e2afdd5ca342b1379a92486339d55d145bc9
Harry <hjwp2@cantab.net>
2019-11-06 13:29:39 +0000

    Use the ORM instead [view_using_orm]

--- src/allocation/views.py
+++ src/allocation/views.py
@@ -1,11 +1,11 @@
+from allocation.domain import model
 from allocation.service_layer import unit_of_work
 
 def allocations(orderid: str, uow: unit_of_work.SqlAlchemyUnitOfWork):
     with uow:
-        products = uow.products.for_order(orderid=orderid)
-        batches = [b for p in products for b in p.batches]
-        return [
-            {'sku': b.sku, 'batchref': b.reference}
-            for b in batches
-            if orderid in b.orderids
-        ]
+        batches = uow.session.query(model.Batch).join(
+            model.OrderLine, model.Batch._allocations
+        ).filter(
+            model.OrderLine.orderid == orderid
+        )
+        return [{'sku': b.sku, 'batchref': b.reference} for b in batches]

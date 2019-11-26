51c336e2e4b71cc6cbc4445933779ecd2933f2de
Harry <hjwp2@cantab.net>
2019-11-26 16:16:46 +0000

    new helpers to update read model [redis_readmodel_client]

--- src/allocation/adapters/redis_eventpublisher.py
+++ src/allocation/adapters/redis_eventpublisher.py
@@ -14,3 +14,11 @@ r = redis.Redis(**config.get_redis_host_and_port())
 def publish(channel, event: events.Event):
     logging.debug('publishing: channel=%s, event=%s', channel, event)
     r.publish(channel, json.dumps(asdict(event)))
+
+
+def update_readmodel(orderid, sku, batchref):
+    r.hset(orderid, sku, batchref)
+
+
+def get_readmodel(orderid):
+    return r.hgetall(orderid)

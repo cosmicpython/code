206081019ea47a63de8a3c772b3ec4961ca629a9
Harry <hjwp2@cantab.net>
2020-01-06 07:32:55 +0000

    fixture to start mappers explicitly, use in repo and uow tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,8 +26,13 @@ def in_memory_sqlite_db():
 
 @pytest.fixture
 def sqlite_session_factory(in_memory_sqlite_db):
-    start_mappers()
     yield sessionmaker(bind=in_memory_sqlite_db)
+
+
+@pytest.fixture
+def mappers():
+    start_mappers()
+    yield
     clear_mappers()
 
 
@@ -57,9 +62,7 @@ def postgres_db():
 
 @pytest.fixture
 def postgres_session_factory(postgres_db):
-    start_mappers()
     yield sessionmaker(bind=postgres_db)
-    clear_mappers()
 
 
 @pytest.fixture

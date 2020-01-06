a8ec47f4848729bc99bb9ef7b3187425cf337c72
Harry <hjwp2@cantab.net>
2020-01-06 22:08:03 +0000

    Uow no longer puts events directly on the bus [uow_collect_new_events]

--- src/allocation/service_layer/unit_of_work.py
+++ src/allocation/service_layer/unit_of_work.py
@@ -8,7 +8,6 @@ from sqlalchemy.orm.session import Session
 
 from allocation import config
 from allocation.adapters import repository
-from . import messagebus
 
 
 class AbstractUnitOfWork(abc.ABC):
@@ -22,13 +21,11 @@ class AbstractUnitOfWork(abc.ABC):
 
     def commit(self):
         self._commit()
-        self.publish_events()
 
-    def publish_events(self):
+    def collect_new_events(self):
         for product in self.products.seen:
             while product.events:
-                event = product.events.pop(0)
-                messagebus.handle(event)
+                yield product.events.pop(0)
 
     @abc.abstractmethod
     def _commit(self):

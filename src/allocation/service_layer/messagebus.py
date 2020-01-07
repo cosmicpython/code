6157d486314c64cc37c78e47d61eff15383f4470
Harry <hjwp2@cantab.net>
2020-01-07 12:44:24 +0000

    top-level messagebus.handle dispatches events vs comannds [messagebus_dispatches_differently]

--- src/allocation/service_layer/messagebus.py
+++ src/allocation/service_layer/messagebus.py
@@ -1,23 +1,33 @@
+# pylint: disable=bare-except
 from __future__ import annotations
-from typing import List, Dict, Callable, Type, TYPE_CHECKING
-from allocation.domain import events
+import logging
+from typing import List, Dict, Callable, Type, Union, TYPE_CHECKING
+from allocation.domain import commands, events
 from . import handlers
 
 if TYPE_CHECKING:
     from . import unit_of_work
 
+logger = logging.getLogger(__name__)
+
+Message = Union[commands.Command, events.Event]
+
 
 def handle(
-    event: events.Event,
+    message: Message,
     uow: unit_of_work.AbstractUnitOfWork,
 ):
     results = []
-    queue = [event]
+    queue = [message]
     while queue:
-        event = queue.pop(0)
-        for handler in HANDLERS[type(event)]:
-            results.append(handler(event, uow=uow))
-            queue.extend(uow.collect_new_events())
+        message = queue.pop(0)
+        if isinstance(message, events.Event):
+            handle_event(message, queue, uow)
+        elif isinstance(message, commands.Command):
+            cmd_result = handle_command(message, queue, uow)
+            results.append(cmd_result)
+        else:
+            raise Exception(f"{message} was not an Event or Command")
     return results
 
 

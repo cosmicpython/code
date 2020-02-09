0182ccf6654f7f4b4ed05d149fa8c3fa3a288f4c
Harry <hjwp2@cantab.net>
2020-02-09 12:02:51 +0000

    use classes in services/handlers test [tests_use_classes]

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -1,3 +1,4 @@
+# pylint: disable=no-self-use
 from unittest import mock
 import pytest
 from allocation.adapters import repository
@@ -31,49 +32,53 @@ class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
 
 
 
-def test_add_batch_for_new_product():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
-    assert uow.products.get("CRUNCHY-ARMCHAIR") is not None
-    assert uow.committed
+class TestAddBatch:
+
+    def test_for_new_product(self):
+        uow = FakeUnitOfWork()
+        services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
+        assert uow.products.get("CRUNCHY-ARMCHAIR") is not None
+        assert uow.committed
 
 
-def test_add_batch_for_existing_product():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "GARISH-RUG", 100, None, uow)
-    services.add_batch("b2", "GARISH-RUG", 99, None, uow)
-    assert "b2" in [b.reference for b in uow.products.get("GARISH-RUG").batches]
+    def test_for_existing_product(self):
+        uow = FakeUnitOfWork()
+        services.add_batch("b1", "GARISH-RUG", 100, None, uow)
+        services.add_batch("b2", "GARISH-RUG", 99, None, uow)
+        assert "b2" in [b.reference for b in uow.products.get("GARISH-RUG").batches]
 
 
-def test_allocate_returns_allocation():
-    uow = FakeUnitOfWork()
-    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
-    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
-    assert result == "batch1"
+class TestAllocate:
+
+    def test_returns_allocation(self):
+        uow = FakeUnitOfWork()
+        services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
+        result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
+        assert result == "batch1"
 
 
-def test_allocate_errors_for_invalid_sku():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "AREALSKU", 100, None, uow)
+    def test_errors_for_invalid_sku(self):
+        uow = FakeUnitOfWork()
+        services.add_batch("b1", "AREALSKU", 100, None, uow)
 
-    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate("o1", "NONEXISTENTSKU", 10, uow)
-
-
-def test_allocate_commits():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
-    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
-    assert uow.committed
+        with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+            services.allocate("o1", "NONEXISTENTSKU", 10, uow)
 
 
-def test_sends_email_on_out_of_stock_error():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "POPULAR-CURTAINS", 9, None, uow)
+    def test_commits(self):
+        uow = FakeUnitOfWork()
+        services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
+        services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+        assert uow.committed
 
-    with mock.patch("allocation.adapters.email.send_mail") as mock_send_mail:
-        services.allocate("o1", "POPULAR-CURTAINS", 10, uow)
-        assert mock_send_mail.call_args == mock.call(
-            "stock@made.com",
-            f"Out of stock for POPULAR-CURTAINS",
-        )
+
+    def test_sends_email_on_out_of_stock_error(self):
+        uow = FakeUnitOfWork()
+        services.add_batch("b1", "POPULAR-CURTAINS", 9, None, uow)
+
+        with mock.patch("allocation.adapters.email.send_mail") as mock_send_mail:
+            services.allocate("o1", "POPULAR-CURTAINS", 10, uow)
+            assert mock_send_mail.call_args == mock.call(
+                "stock@made.com",
+                f"Out of stock for POPULAR-CURTAINS",
+            )

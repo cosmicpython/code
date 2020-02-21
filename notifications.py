0c2e17bcc49ccc36b5643aa48c05f9b7cc0a4c9b
Harry <hjwp2@cantab.net>
2020-02-21 14:34:09 +0000

    flesh it out a bit

---
+++ notifications.py
@@ -0,0 +1,5 @@
+def notify_delay(*args, **kwargs):
+    pass
+
+def notify_new_large_shipment(*args, **kwargs):
+    pass
--- sync_shipments.py
+++ sync_shipments.py
@@ -1,9 +1,11 @@
 import logging
 from dataclasses import dataclass
 from datetime import date
-from typing import List
+from typing import List, Optional
 import requests
 import requests.exceptions
+
+from notifications import notify_delay, notify_new_large_shipment
 
 API_URL = 'https://example.org'
 
@@ -17,27 +19,56 @@ class OrderLine:
 class Shipment:
     reference: str
     lines: List[OrderLine]
-    eta: date
+    eta: Optional[date]
+    incoterm: str
 
 
-def update_shipment(shipment):
+def set_eta(shipment, eta):
+    logging.info(
+        'setting new shipment eta for %s: %s (was %s)',
+        shipment.reference, eta, shipment.eta
+    )
+    if shipment.eta is not None and eta > shipment.eta:
+        notify_delay(shipment_ref=shipment.reference, delay=eta - shipment.eta)
+    if shipment.eta is None and shipment.incoterm == 'FOB' and len(shipment.lines) > 10:
+        notify_new_large_shipment(shipment_ref=shipment.reference, eta=eta)
+
+    shipment.eta = eta
+    sync_to_api(shipment)
+
+
+
+def sync_to_api(shipment):
     external_shipment_id = get_shipment_id(shipment.reference)
-    requests.put(f'{API_URL}/shipment/{external_shipment_id}', json={
-        'client_reference': shipment.reference,
-        'arrival_date': shipment.eta,
-        'contents': [
-            {'sku': ol.sku, 'quantity': ol.quantity}
-            for ol in shipment.lines
-        ]
-    })
+    if external_shipment_id is None:
+        requests.post(f'{API_URL}/shipments/', json={
+            'client_reference': shipment.reference,
+            'arrival_date': shipment.eta.isoformat()[:10],
+            'products': [
+                {'sku': ol.sku, 'quantity': ol.quantity}
+                for ol in shipment.lines
+            ]
+        })
+
+    else:
+        requests.put(f'{API_URL}/shipments/{external_shipment_id}', json={
+            'client_reference': shipment.reference,
+            'arrival_date': shipment.eta.isoformat()[:10],
+            'products': [
+                {'sku': ol.sku, 'quantity': ol.quantity}
+                for ol in shipment.lines
+            ]
+        })
 
 
-def get_shipment_id(our_reference):
+def get_shipment_id(our_reference) -> Optional[str]:
     try:
-        their_shipments = requests.get(f"{API_URL}/shipment/").json()['items']
-        return next(s for s in their_shipments if s['client_reference'] == our_reference)
-    except StopIteration:
-        logging.error('No shipment found with reference %s', our_reference)
+        their_shipments = requests.get(f"{API_URL}/shipments/").json()['items']
+        return next(
+            (s for s in their_shipments if s['client_reference'] == our_reference),
+            None
+        )
 
     except requests.exceptions.RequestException:
         logging.exception('Error retrieving shipment')
+        raise

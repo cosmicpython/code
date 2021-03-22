9ac7dd0826488aca8ad81488ba2ffca4095b8ed2
Harry <hjwp2@cantab.net>
2021-03-22 22:53:57 +0000

    Revert "alternative approach using DI and FakeFilesystem" [chapter_03_abstractions_ends]
    This reverts commit 832bab776f136611c6a18ad689bdc09e21989ade.

--- test_sync.py
+++ test_sync.py
@@ -1,7 +1,7 @@
 import tempfile
 from pathlib import Path
 import shutil
-from sync import FileSystem, sync
+from sync import sync, determine_actions
 
 
 class TestE2E:
@@ -47,38 +47,15 @@ class TestE2E:
             shutil.rmtree(dest)
 
 
-class FakeFilesystem:
-    def __init__(self, path_hashes):
-        self.path_hashes = path_hashes
-        self.actions = []
-
-    def read(self, path):
-        return self.path_hashes[path]
-
-    def copy(self, src, dest):
-        self.actions.append(('COPY', src, dest))
-
-    def move(self, src, dest):
-        self.actions.append(('MOVE', src, dest))
-
-    def delete(self, dest):
-        self.actions.append(('DELETE', dest))
-
-
-
 def test_when_a_file_exists_in_the_source_but_not_the_destination():
-    fakefs = FakeFilesystem({
-        '/src': {"hash1": "fn1"},
-        '/dst': {},
-    })
-    sync('/src', '/dst', filesystem=fakefs)
-    assert fakefs.actions == [("COPY", Path("/src/fn1"), Path("/dst/fn1"))]
+    source_hashes = {"hash1": "fn1"}
+    dest_hashes = {}
+    actions = determine_actions(source_hashes, dest_hashes, Path("/src"), Path("/dst"))
+    assert list(actions) == [("COPY", Path("/src/fn1"), Path("/dst/fn1"))]
 
 
 def test_when_a_file_has_been_renamed_in_the_source():
-    fakefs = FakeFilesystem({
-        '/src': {"hash1": "fn1"},
-        '/dst': {"hash1": "fn2"},
-    })
-    sync('/src', '/dst', filesystem=fakefs)
-    assert fakefs.actions == [("MOVE", Path("/dst/fn2"), Path("/dst/fn1"))]
+    source_hashes = {"hash1": "fn1"}
+    dest_hashes = {"hash1": "fn2"}
+    actions = determine_actions(source_hashes, dest_hashes, Path("/src"), Path("/dst"))
+    assert list(actions) == [("MOVE", Path("/dst/fn2"), Path("/dst/fn1"))]

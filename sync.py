f19f8d2236331976b5a55b14ddf88f6b340ae881
Harry <hjwp2@cantab.net>
2021-03-22 22:29:06 +0000

    alternative approach using DI and FakeFilesystem

--- sync.py
+++ sync.py
@@ -3,23 +3,40 @@ import os
 import shutil
 from pathlib import Path
 
+class FileSystem:
 
-def sync(source, dest):
-    # imperative shell step 1, gather inputs
-    source_hashes = read_paths_and_hashes(source)
-    dest_hashes = read_paths_and_hashes(dest)
+    def read(self, path):
+        return read_paths_and_hashes(path)
 
-    # step 2: call functional core
-    actions = determine_actions(source_hashes, dest_hashes, source, dest)
+    def copy(self, source, dest):
+        shutil.copyfile(source, dest)
 
-    # imperative shell step 3, apply outputs
-    for action, *paths in actions:
-        if action == "COPY":
-            shutil.copyfile(*paths)
-        if action == "MOVE":
-            shutil.move(*paths)
-        if action == "DELETE":
-            os.remove(paths[0])
+    def move(self, source, dest):
+        shutil.move(source, dest)
+
+    def delete(self, dest):
+        os.remove(dest)
+
+
+def sync(source, dest, filesystem=FileSystem()):
+    source_hashes = filesystem.read(source)
+    dest_hashes = filesystem.read(dest)
+
+    for sha, filename in source_hashes.items():
+        if sha not in dest_hashes:
+            sourcepath = Path(source) / filename
+            destpath = Path(dest) / filename
+            filesystem.copy(sourcepath, destpath)
+
+        elif dest_hashes[sha] != filename:
+            olddestpath = Path(dest) / dest_hashes[sha]
+            newdestpath = Path(dest) / filename
+            filesystem.move(olddestpath, newdestpath)
+
+    for sha, filename in dest_hashes.items():
+        if sha not in source_hashes:
+            filesystem.delete(dest / filename)
+
 
 
 BLOCKSIZE = 65536
@@ -41,20 +58,3 @@ def read_paths_and_hashes(root):
         for fn in files:
             hashes[hash_file(Path(folder) / fn)] = fn
     return hashes
-
-
-def determine_actions(source_hashes, dest_hashes, source_folder, dest_folder):
-    for sha, filename in source_hashes.items():
-        if sha not in dest_hashes:
-            sourcepath = Path(source_folder) / filename
-            destpath = Path(dest_folder) / filename
-            yield "COPY", sourcepath, destpath
-
-        elif dest_hashes[sha] != filename:
-            olddestpath = Path(dest_folder) / dest_hashes[sha]
-            newdestpath = Path(dest_folder) / filename
-            yield "MOVE", olddestpath, newdestpath
-
-    for sha, filename in dest_hashes.items():
-        if sha not in source_hashes:
-            yield "DELETE", dest_folder / filename

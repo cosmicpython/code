8c329409b79230da56a8e23dbffdbf73ce613061
ANDERSON WEBER <andweber92@gmail.com>
2022-03-13 20:10:32 -0300

    Inserted lines on model.py

---
+++ model.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import date
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    order_id: str
+    sku: str
+    qty: int
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self.available_quantity = qty
+
+    def allocate(self, line: OrderLine):
+        self.available_quantity -= line.qty

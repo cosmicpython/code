63dc5d9eaf93e14530d512d1b4d3c462d54decdc
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-09-22 15:33:31 -0400

    add can_allocate method with @mdelgadonyc

--- model.py
+++ model.py
@@ -6,22 +6,23 @@ from typing import Optional, Dict
 # @define
 class Batch:
 
-	def __init__(self, ref:str, sku:str, qty:int, eta:date):
-		self.reference = ref
-		self.sku = sku
-		self.eta = eta
-		self.available_quantity = qty 
+    def __init__(self, ref: str, sku: str, qty: int, eta: date):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self.available_quantity = qty
 
-	def allocate(self, line):
-		self.available_quantity -= line.qty
+    def allocate(self, line):
+        self.available_quantity -= line.qty
 
-
+    def can_allocate(self, line) -> bool:
+        return self.available_quantity >= line.qty
 
 
 # @define
 class OrderLine:
 
-	def __init__(self, order_ref:str, sku:str, qty:int):
-		self.orderid = order_ref 
-		self.sku = sku 
-		self.qty = qty 
+    def __init__(self, order_ref: str, sku: str, qty: int):
+        self.orderid = order_ref
+        self.sku = sku
+        self.qty = qty
--- test_model.py
+++ test_model.py
@@ -8,21 +8,23 @@ tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
 
-def test_allocating_to_a_batch_reduces_the_available_quantity():
-    # pytest.fail("todo")
-    batch = Batch("1", "TABLE", qty=20, eta=date.today())
-    line = OrderLine("order-ref", "TABLE", 2)
+def make_batch_and_line(sku: str, batch_qty: int, line_qty: int):
+    return (Batch("1", sku, batch_qty, eta=date.today()),
+            OrderLine("order-ref", sku, line_qty))
 
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch, line = make_batch_and_line("TABLE", 20, 2)
     batch.allocate(line)
 
-    assert batch.available_quantity == 18
-
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
+    large_batch, small_line = make_batch_and_line("chair", 20, 2)
+    assert large_batch.can_allocate(small_line)
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+    small_batch, large_line = make_batch_and_line("chair", 2, 20)
+    assert not small_batch.can_allocate(large_line)
 
 
 def test_can_allocate_if_available_equal_to_required():

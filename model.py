0ae3dda014462dc907ae17913d2199409f7470ea
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-09-22 14:33:33 -0400

    add allocate batch functionality

---
+++ model.py
@@ -0,0 +1,27 @@
+# from attrs import define
+from datetime import date
+from typing import Optional, Dict
+
+
+# @define
+class Batch:
+
+	def __init__(self, ref:str, sku:str, qty:int, eta:date):
+		self.reference = ref
+		self.sku = sku
+		self.eta = eta
+		self.available_quantity = qty 
+
+	def allocate(self, line):
+		self.available_quantity -= line.qty
+
+
+
+
+# @define
+class OrderLine:
+
+	def __init__(self, order_ref:str, sku:str, qty:int):
+		self.orderid = order_ref 
+		self.sku = sku 
+		self.qty = qty 
--- test_model.py
+++ test_model.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 import pytest
 
-# from model import ...
+from model import Batch, OrderLine
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
@@ -9,8 +9,13 @@ later = tomorrow + timedelta(days=10)
 
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail("todo")
+    # pytest.fail("todo")
+    batch = Batch("1", "TABLE", qty=20, eta=date.today())
+    line = OrderLine("order-ref", "TABLE", 2)
 
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
 
 def test_can_allocate_if_available_greater_than_required():
     pytest.fail("todo")
